4c18617f7af23f9411f000d2afb67bdd57425723
Dina-Abd-Elrahman <dena.elsayad@gmail.com>
2018-12-31 14:24:13 +0200

    add  test cases

--- Jumpscale/sal/ssl/tests/test_ssl.py
+++ Jumpscale/sal/ssl/tests/test_ssl.py
@@ -1,13 +1,45 @@
+import subprocess 
 from Jumpscale import j
+from Jumpscale.sal.ssl.SSLFactory import SSLFactory
 
-def test_main(self=None):
+SSLFactory = SSLFactory()
+
+def test_ca_generate():
     
     """
     to run
-    js.shell 'j.sal.ssl.ca_cert_generate()'
+    js.shell 'j.sal.ssl.ca_cert_generate(cert_dir='/test')'
+    CA (Certificate Authority) generate
+    """
+    
+    SSLFactory.ca_cert_generate('/tmp/test/')
+    file_list = subprocess.run('ls -la /tmp/test/', shell=True, stdout = subprocess.PIPE, stderr = subprocess.PIPE) 
+    files = file_list.stdout.decode()
+    assert ca.key in files
+    assert ca.crt in files
+
+def test_verify():
+
+    """
+        to run
+        js.shell 'j.sal.ssl.verify('/tmp/test/ca.crt', '/tmp/test/ca.key')
     """
 
-    
+    output = SSLFactory.verify('/tmp/test/ca.crt', '/tmp/test/ca.key')
+    assert output is True
 
+def test_certificate_signing_request_create():
+
+    """
+       to run 
+       js.shell 'j.sal.ssl.certificate_signing_request_create('test')'
+
+    """
+
+    output = SSLFactory.certificate_signing_request_create('test')
+    assert "BEGIN PRIVATE KEY" in str(output)
+    assert "END PRIVATE KEY" in str(output)
+    assert "BEGIN CERTIFICATE REQUEST" in str(output)
+    assert "END CERTIFICATE REQUEST" in str(output)
 
 
