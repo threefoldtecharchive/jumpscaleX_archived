048b07f095775bb7bb4584df02b566aad0028492
Abdulrahman Hamdy <arahman.hamdy91@gmail.com>
2019-01-08 12:17:57 +0200

    Fix docsite test

--- Jumpscale/tools/markdowndocs/DocSite.py
+++ Jumpscale/tools/markdowndocs/DocSite.py
@@ -567,7 +567,8 @@ class DocSite(j.application.JSBaseClass):
 
         for key, doc in self.docs.items():
             # doc.defs_process()
-            doc.write()
+            if doc:
+                doc.write()
 
         # if j.sal.fs.exists(j.sal.fs.joinPaths(self.path, "static"), followlinks=True):
         #     j.sal.fs.copyDirTree(j.sal.fs.joinPaths(self.path, "static"), j.sal.fs.joinPaths(self.outpath, "public"))
