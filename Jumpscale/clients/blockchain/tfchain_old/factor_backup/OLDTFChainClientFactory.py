ad98d2d8a6d92ccd017e80ef8f3525d55b342a51
Glen De Cauwsemaecker <contact@glendc.com>
2019-01-24 14:41:13 +0100

    remove namespace from old tfchain factory

--- Jumpscale/clients/blockchain/tfchain_old/factor_backup/OLDTFChainClientFactory.py
+++ Jumpscale/clients/blockchain/tfchain_old/factor_backup/OLDTFChainClientFactory.py
@@ -33,7 +33,7 @@ class TfchainClientFactory(j.application.JSBaseConfigsClass):
     """
     Factory class to get a tfchain client object
     """
-    __jslocation__ = "j.clients.tfchain"
+   # __jslocation__ = "j.clients.tfchain"
     #_CHILDCLASS = TFChainClient
 
 
