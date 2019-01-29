d13fe5feb07a019cd2ae601ac5cfd3ff5e05a156
Reem Khamis <reem.khamis@gmail.com>
2019-01-29 15:11:23 +0200

    Fix BuildEnv

--- Jumpscale/builder/buildenv/BuildEnv.py
+++ Jumpscale/builder/buildenv/BuildEnv.py
@@ -37,34 +37,20 @@ class BuildEnv(j.builder.system._BaseFactoryClass):
         if j.core.platformtype.myplatform.isMac:
             C = ""
         else:
-            C = """
-            sudo
-            net-tools
-            python3
-            python3-distutils
-            python3-psutil
-            """
+            C = "sudo net-tools python3 python3-distutils python3-psutil"
 
-        C += """
-        openssl
-        wget
-        curl
-        git
-        mc
-        tmux
-        rsync
-        """
+        C += " openssl wget curl git mc tmux rsync"
         j.builder.tools.package_install(C)
 
-        ##j.builder.sandbox.profileJS.addPath("{DIR_BIN}")
-        ##j.builder.sandbox.profileJS.save()
+        # j.builder.sandbox.profileJS.addPath("{DIR_BIN}")
+        # j.builder.sandbox.profileJS.save()
 
         if upgrade:
             self.upgrade(reset=reset, update=False)
 
         self._done_set("install")
 
-    def development(self,reset=False,python=False):
+    def development(self, reset=False, python=False):
         """
         install all components required for building (compiling)
 
@@ -82,30 +68,29 @@ class BuildEnv(j.builder.system._BaseFactoryClass):
         pkg-config
         curl
         """
-        C=j.core.text.strip(C)
+        C = j.core.text.strip(C)
 
         if j.core.platformtype.myplatform.isMac:
 
-            if  not self._done_get("xcode_install"):
+            if not self._done_get("xcode_install"):
                 j.sal.process.execute("xcode-select --install", die=False, showout=True)
-                cmd="sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /"
+                cmd = "sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /"
                 j.sal.process.execute(cmd, die=False, showout=True)
                 self._done_set("xcode_install")
 
-
-            C+="libffi "
-            C+="automake "
-            C+="pcre "
-            C+="xz "
-            C+="openssl "
-            C+="zlib "
+            C += "libffi "
+            C += "automake "
+            C += "pcre "
+            C += "xz "
+            C += "openssl "
+            C += "zlib "
         else:
-            C+="libffi-dev "
-            C+="build-essential "
-            C+="libsqlite3-dev "
-            C+="libpq-dev "
+            C += "libffi-dev "
+            C += "build-essential "
+            C += "libsqlite3-dev "
+            C += "libpq-dev "
             if python:
-                C+="python3-dev "
+                C += "python3-dev "
 
         self.install()
         if self._done_check("development", reset):
@@ -122,7 +107,3 @@ class BuildEnv(j.builder.system._BaseFactoryClass):
         j.builder.system.package.clean()
 
         self._done_set("upgrade")
-
-
-
-
