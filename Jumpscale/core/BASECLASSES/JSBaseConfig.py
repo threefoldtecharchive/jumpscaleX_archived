7df458e9333da84f504e3aaadc4a8969361c43b3
Reem Khamis <reem.khamis@gmail.com>
2019-01-29 19:34:24 +0200

    Fix data_update

--- Jumpscale/core/BASECLASSES/JSBaseConfig.py
+++ Jumpscale/core/BASECLASSES/JSBaseConfig.py
@@ -7,17 +7,16 @@ class JSBaseConfig(JSBase):
 
     def _class_init(self):
 
-        if not hasattr(self.__class__,"_class_init_done"):
+        if not hasattr(self.__class__, "_class_init_done"):
 
-            if not hasattr(self.__class__,"_SCHEMATEXT"):
-                raise RuntimeError("need _SCHEMATEXT as class property.\n%s"%(HELPTEXT))
+            if not hasattr(self.__class__, "_SCHEMATEXT"):
+                raise RuntimeError("need _SCHEMATEXT as class property.\n%s" % (HELPTEXT))
 
             JSBase._class_init(self)
 
             # print("classinit:%s"%self.__class__)
 
-
-    def __init__(self,data=None, parent=None, **kwargs):
+    def __init__(self, data=None, parent=None, **kwargs):
         """
         :param data, is a jsobject as result of jsX schema's
         :param factory, don't forget to specify this
@@ -26,13 +25,13 @@ class JSBaseConfig(JSBase):
         the self.data object is a jsobject (result of using the jsx schemas)
 
         """
-        self._class_init() #is needed to init class properties, needs to be first thing
-        JSBase.__init__(self,init=False)
+        self._class_init()  # is needed to init class properties, needs to be first thing
+        JSBase.__init__(self, init=False)
 
         self._parent = parent
 
         if data:
-            if not hasattr(data,"_JSOBJ"):
+            if not hasattr(data, "_JSOBJ"):
                 raise RuntimeError("data should be a jsobj")
             self.data = data
             self.data_update(**kwargs)
@@ -41,12 +40,10 @@ class JSBaseConfig(JSBase):
 
         self._init()
 
-        if "name" not in  self.data._ddict:
+        if "name" not in self.data._ddict:
             raise RuntimeError("name needs to be specified in data")
 
-        self._key = "%s_%s"%(self.__class__.__name__,self.data.name)
-
-
+        self._key = "%s_%s" % (self.__class__.__name__, self.data.name)
 
     def _obj_cache_reset(self):
         """
@@ -74,14 +71,16 @@ class JSBaseConfig(JSBase):
     def save(self):
         self.data.save()
 
-    def data_update(self,**kwargs):
+    def data_update(self, **kwargs):
         """
         will not automatically save the data, don't forget to call self.save()
-         
+
         :param kwargs:
         :return:
         """
         self.data.data_update(data=kwargs)
+        for prop, val in self.data._ddict.items():
+            setattr(self, prop, val)
         # self.data.save()
 
     def _data_trigger_new(self):
@@ -110,22 +109,22 @@ class JSBaseConfig(JSBase):
         :return:
 
         """
-        path = j.core.tools.text_replace("{DIR_TEMP}/js_baseconfig_%s.toml"%self.__class__._location)
+        path = j.core.tools.text_replace("{DIR_TEMP}/js_baseconfig_%s.toml" % self.__class__._location)
         data_in = self.data._toml
-        j.sal.fs.writeFile(path,data_in)
+        j.sal.fs.writeFile(path, data_in)
         j.core.tools.file_edit(path)
         data_out = j.sal.fs.readFile(path)
         if data_in != data_out:
-            self._logger.debug("'%s' instance '%s' has been edited (changed)"%(self._parent.__jslocation__,self.data.name))
+            self._logger.debug("'%s' instance '%s' has been edited (changed)" %
+                               (self._parent.__jslocation__, self.data.name))
             data2 = j.data.serializers.toml.loads(data_out)
             self.data.data_update(data2)
         j.sal.fs.remove(path)
 
     def view(self):
-        path = j.core.tools.text_replace("{DIR_TEMP}/js_baseconfig_%s.toml"%self.__class__._location)
+        path = j.core.tools.text_replace("{DIR_TEMP}/js_baseconfig_%s.toml" % self.__class__._location)
         data_in = self.data._toml
         j.tools.formatters.print_toml(data_in)
-
 
     def __getattr__(self, attr):
         if attr.startswith("_"):
@@ -134,30 +133,27 @@ class JSBaseConfig(JSBase):
             return self.data.__getattribute__(attr)
         return self.__getattribute__(attr)
 
-
     def _properties_model(self):
         return self._model.schema.propertynames
 
-
     def __setattr__(self, key, value):
-        if key.startswith("_") or key=="data":
-            self.__dict__[key]=value
+        if key.startswith("_") or key == "data":
+            self.__dict__[key] = value
             return
 
         if "data" in self.__dict__ and key in self._model.schema.propertynames:
             # if value != self.data.__getattribute__(key):
-            self._logger.debug("SET:%s:%s"%(key,value))
-            self.__dict__["data"].__setattr__(key,value)
+            self._logger.debug("SET:%s:%s" % (key, value))
+            self.__dict__["data"].__setattr__(key, value)
 
-        self.__dict__[key]=value
-
+        self.__dict__[key] = value
 
     def __str__(self):
         try:
-            out = "%s\n%s\n"%(self.__class__.__name__,self.data)
+            out = "%s\n%s\n" % (self.__class__.__name__, self.data)
         except:
             out = str(self.__class__)+"\n"
-            out+=j.core.text.prefix(" - ", self.data)
+            out += j.core.text.prefix(" - ", self.data)
         return out
 
     __repr__ = __str__
