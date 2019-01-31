626dad656e4233332bd44f1184caf25fe479248b
Glen De Cauwsemaecker <contact@glendc.com>
2019-01-31 17:50:53 +0100

    Revert "make TFChainClient async for wallet balance checking"
    This reverts commit f612bb05fb8847c928cacb43f080145f4803252c.

--- Jumpscale/clients/blockchain/tfchain/TFChainClient.py
+++ Jumpscale/clients/blockchain/tfchain/TFChainClient.py
@@ -3,14 +3,10 @@ Tfchain Client
 """
 
 from Jumpscale import j
-
-import multiprocessing
-
 from .TFChainWalletFactory import TFChainWalletFactory
 from .types.ConditionTypes import UnlockHash, UnlockHashType
 from .types.PrimitiveTypes import Hash, Currency
 from .types.CompositionTypes import CoinOutput
-from .types.errors import ExplorerNoContent
 from .TFChainTransactionFactory import TransactionBaseClass
 
 _EXPLORER_NODES = {
@@ -29,38 +25,20 @@ _EXPLORER_NODES = {
     ],
 }
 
+
 class TFChainClient(j.application.JSBaseConfigParentClass):
     """
-    Tfchain client object.
-
-       NOTE about multiprocessing.
-    On MacOS Sierra and higher you need to export the following variable
-    in order to make use of this Client's multiprocessing features:
-        ```
-        export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-        ```
-    You can also disable the multiprocessing feature by setting the property
-    max_workers of the client instance to `1`, in which case you do not
-    need to export the variable above.
+    Tfchain client object
     """
-
     _SCHEMATEXT = """
         @url = jumpscale.tfchain.client
         name* = "" (S)
         network_type = "STD,TEST,DEV" (E)
         minimum_minerfee = 100000000 (I)
         explorer_nodes = (LS) !jumpscale.tfchain.explorer
-
-        max_workers = 0 (I)
         """
 
     _CHILDCLASSES = [TFChainWalletFactory]
-
-    def _init(self):
-        # by default use the network explorers, can be overriden (e.g. for testing purposes)
-        from .TFChainExplorerClient import get, post
-        self._explorer_get = get
-        self._explorer_post = post
 
     def _data_trigger_new(self):
         if self.network_type == "DEV":
@@ -81,7 +59,7 @@ class TFChainClient(j.application.JSBaseConfigParentClass):
         """
         Get the current blockchain info, using the last known block, as reported by an explorer.
         """
-        resp = self._explorer_get(addresses=self.explorer_addresses, endpoint="/explorer")
+        resp = self._explorer_get(endpoint="/explorer")
         resp = j.data.serializers.json.loads(resp)
         blockid = Hash.from_json(obj=resp['blockid'])
         last_block = self.block_get(blockid)
@@ -95,12 +73,12 @@ class TFChainClient(j.application.JSBaseConfigParentClass):
         """
         # get the explorer block
         if isinstance(value, int):
-            resp = self._explorer_get(addresses=self.explorer_addresses, endpoint="/explorer/blocks/{}".format(int(value)))
+            resp = self._explorer_get(endpoint="/explorer/blocks/{}".format(int(value)))
             resp = j.data.serializers.json.loads(resp)
             resp = resp['block']
         else:
-            blockid = _normalize_id(value)
-            resp = self._explorer_get(addresses=self.explorer_addresses, endpoint="/explorer/hashes/"+blockid)
+            blockid = self._normalize_id(value)
+            resp = self._explorer_get(endpoint="/explorer/hashes/"+blockid)
             resp = j.data.serializers.json.loads(resp)
             assert resp['hashtype'] == 'blockid'
             resp = resp['block']
@@ -109,7 +87,7 @@ class TFChainClient(j.application.JSBaseConfigParentClass):
         transactions = []
         for etxn in resp['transactions']:
             # parse the explorer transaction
-            transaction = _transaction_from_explorer_transaction(etxn)
+            transaction = self._transaction_from_explorer_transaction(etxn)
             # append the transaction to the list of transactions
             transactions.append(transaction)
         rawblock = resp['rawblock']
@@ -142,13 +120,13 @@ class TFChainClient(j.application.JSBaseConfigParentClass):
         
         @param txid: the identifier (fixed string with a length of 64) that points to the desired transaction
         """
-        txid = _normalize_id(txid)
-        resp = self._explorer_get(addresses=self.explorer_addresses, endpoint="/explorer/hashes/"+txid)
+        txid = self._normalize_id(txid)
+        resp = self._explorer_get(endpoint="/explorer/hashes/"+txid)
         resp = j.data.serializers.json.loads(resp)
         assert resp['hashtype'] == 'transactionid'
         resp = resp['transaction']
         assert resp['id'] == txid
-        return _transaction_from_explorer_transaction(resp)
+        return self._transaction_from_explorer_transaction(resp)
 
     def transaction_put(self, transaction):
         """
@@ -158,29 +136,9 @@ class TFChainClient(j.application.JSBaseConfigParentClass):
         """
         if isinstance(transaction, TransactionBaseClass):
             transaction = transaction.json()
-        resp = self._explorer_post(addresses=self.explorer_addresses, endpoint="/transactionpool/transactions", data=transaction)
+        resp = self._explorer_post(endpoint="/transactionpool/transactions", data=transaction)
         resp = j.data.serializers.json.loads(resp)
         return Hash(value=resp.get('transactionid'))
-
-    def unlockhash_get_all(self, *unlockhashes):
-        """
-        Get all transactions linked to all the given unlockhashes,
-        as well as other information such as the multisig addresses linked to the given unlockhashes.
-
-        Returns a list, where each element is an Async object,
-        you'll need to use the `.get()` method on each element in the list,
-        to get the results for each unlockhash, one element per unlockhash.
-
-        example: [result.get() for result in unlockhash_get_all(hashes)]
-
-        @param unlockhash: the unlockhash to look up transactions for in the explorer
-        """
-        if self.max_workers == 1:
-            # if workers is 1, disable async and return in a vanilla-approach
-            return [_SingleWorkerResult(result=self.unlockhash_get(unlockhash)) for unlockhash in unlockhashes]
-        # create our multiprocessing pool and process all at once
-        pool = self._mp_pool_new()
-        return [pool.apply_async(_unlockhash_get, args=(self._explorer_get,self.explorer_addresses, unlockhash)) for unlockhash in unlockhashes]
     
     def unlockhash_get(self, unlockhash):
         """
@@ -189,45 +147,15 @@ class TFChainClient(j.application.JSBaseConfigParentClass):
 
         @param unlockhash: the unlockhash to look up transactions for in the explorer
         """
-        return _unlockhash_get(self._explorer_get, self.explorer_addresses, unlockhash)
-
-    def _mp_pool_new(self):
-        """
-        Create a new multiprocessing pool,
-        only used when multiprocessing is enabled.
-        """
-        processes = self.max_workers
-        if processes <= 0:
-            processes = multiprocessing.cpu_count()*2
-        return multiprocessing.Pool(processes=processes)
-
-
-# class to maintain the API of an async result,
-# without relying on the async library.
-class _SingleWorkerResult():
-    def __init__(self, result):
-        self._result = result
-
-    def get(self):
-        return self._result
-
-
-####################################
-### PRIVATE UTILITY FUNCTIONS
-####################################
-
-
-def _unlockhash_get(getter, addresses, unlockhash):
-    try:
-        unlockhash = _normalize_unlockhash(unlockhash)
-        resp = getter(addresses=addresses, endpoint="/explorer/hashes/"+unlockhash)
+        unlockhash = self._normalize_unlockhash(unlockhash)
+        resp = self._explorer_get(endpoint="/explorer/hashes/"+unlockhash)
         resp = j.data.serializers.json.loads(resp)
         assert resp['hashtype'] == 'unlockhash'
         # parse the transactions
         transactions = []
         for etxn in resp['transactions']:
             # parse the explorer transaction
-            transaction = _transaction_from_explorer_transaction(etxn)
+            transaction = self._transaction_from_explorer_transaction(etxn)
             # append the transaction to the list of transactions
             transactions.append(transaction)
         # collect all multisig addresses
@@ -240,50 +168,57 @@ def _unlockhash_get(getter, addresses, unlockhash):
             unlockhash=UnlockHash.from_json(unlockhash),
             transactions=transactions,
             multisig_addresses=multisig_addresses)
-    except ExplorerNoContent:
-        return ExplorerUnlockhashResult(unlockhash=UnlockHash.from_json(unlockhash))
+    
+    def _transaction_from_explorer_transaction(self, etxn):
+        # parse the transactions
+        transaction = j.clients.tfchain.transactions.from_json(obj=etxn['rawtransaction'], id=etxn['id'])
+        # add the parent (coin) outputs
+        coininputoutputs = etxn.get('coininputoutputs', None) or []
+        assert len(transaction.coin_inputs) == len(coininputoutputs)
+        for (idx, co) in enumerate(coininputoutputs):
+            transaction.coin_inputs[idx].parent_output = CoinOutput.from_json(obj=co)
+        # add the coin output ids
+        coinoutputids = etxn.get('coinoutputids', None) or []
+        assert len(transaction.coin_outputs) == len(coinoutputids)
+        for (idx, id) in enumerate(coinoutputids):
+            transaction.coin_outputs[idx].id = Hash.from_json(obj=id)
+        # set the unconfirmed state
+        transaction.unconfirmed = etxn.get('unconfirmed', False)
+        # return the transaction
+        return transaction
 
-def _normalize_unlockhash(unlockhash):
-    if isinstance(unlockhash, UnlockHash):
-        return str(unlockhash)
+    def _explorer_get(self, endpoint):
+        """
+        Private utility method that gets the data on the given endpoint,
+        but in a method so it can be overriden for Testing purposes.
+        """
+        return j.clients.tfchain.explorer.get(addresses=self.explorer_addresses, endpoint=endpoint)
 
-    if isinstance(unlockhash, (bytes, bytearray)):
-        unlockhash = unlockhash.hex()
-    elif isinstance(unlockhash, str):
-        unlockhash = str(unlockhash)
-    else:
-        raise TypeError("Unlock hash has to be a string-like or Hash type")
+    def _explorer_post(self, endpoint, data):
+        """
+        Private utility method that sets the data on the given endpoint,
+        but in a method so it can be overriden for Testing purposes.
+        """
+        return j.clients.tfchain.explorer.post(addresses=self.explorer_addresses, endpoint=endpoint, data=data)
 
-    return str(UnlockHash.from_json(unlockhash))
+    def _normalize_unlockhash(self, unlockhash):
+        if isinstance(unlockhash, UnlockHash):
+            return str(unlockhash)
 
-def _normalize_id(id):
-    if isinstance(id, Hash):
+        if isinstance(unlockhash, (bytes, bytearray)):
+            unlockhash = unlockhash.hex()
+        elif isinstance(unlockhash, str):
+            unlockhash = str(unlockhash)
+        else:
+            raise TypeError("Unlock hash has to be a string-like or Hash type")
+
+        return str(UnlockHash.from_json(unlockhash))
+
+    def _normalize_id(self, id):
+        if isinstance(id, Hash):
+            return str(id)
+        id = Hash(value=id)
         return str(id)
-    id = Hash(value=id)
-    return str(id)
-
-def _transaction_from_explorer_transaction(etxn):
-    # parse the transactions
-    transaction = j.clients.tfchain.transactions.from_json(obj=etxn['rawtransaction'], id=etxn['id'])
-    # add the parent (coin) outputs
-    coininputoutputs = etxn.get('coininputoutputs', None) or []
-    assert len(transaction.coin_inputs) == len(coininputoutputs)
-    for (idx, co) in enumerate(coininputoutputs):
-        transaction.coin_inputs[idx].parent_output = CoinOutput.from_json(obj=co)
-    # add the coin output ids
-    coinoutputids = etxn.get('coinoutputids', None) or []
-    assert len(transaction.coin_outputs) == len(coinoutputids)
-    for (idx, id) in enumerate(coinoutputids):
-        transaction.coin_outputs[idx].id = Hash.from_json(obj=id)
-    # set the unconfirmed state
-    transaction.unconfirmed = etxn.get('unconfirmed', False)
-    # return the transaction
-    return transaction
-
-
-####################################
-### Client Explorer Result Types
-####################################
 
 
 class ExplorerBlockchainInfo():
@@ -324,13 +259,13 @@ class ExplorerBlockchainInfo():
 
 
 class ExplorerUnlockhashResult():
-    def __init__(self, unlockhash, transactions=None, multisig_addresses=None):
+    def __init__(self, unlockhash, transactions, multisig_addresses):
         """
         All the info found for a given unlock hash, as reported by an explorer.
         """
         self._unlockhash = unlockhash
-        self._transactions = transactions or []
-        self._multisig_addresses = multisig_addresses or []
+        self._transactions = transactions
+        self._multisig_addresses = multisig_addresses
     
     @property
     def unlockhash(self):
