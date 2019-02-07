42c1a1b269294781cffc8f028c5722b350568fbe
AhmedHanafy725 <ahmed.hanafy725@gmail.com>
2019-02-07 09:57:31 +0200

    fix script

--- scripts/autotest.py
+++ scripts/autotest.py
@@ -1,6 +1,25 @@
 from Jumpscale import j
+from subprocess import run, PIPE
+import re
 
-cl = j.clients.telegram_bot.get("test", bot_token_="626574765:AAGD5c949N3-za-CtUSba2aXQy-4ThhoD3s")
-cl.send_message(chatid="@hamadatest", text="Hello from OVH")
+ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+client = j.clients.telegram_bot.get("test")
 
+response = run('python3 /sandbox/code/github/threefoldtech/jumpscaleX/test.py', shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE)
+response.stderr = ansi_escape.sub('', response.stderr)
 
+lines = response.stderr.splitlines() 
+lines_send = []
+lines_to_send = ''
+for line in lines:
+    if line.startswith('Traceback'):
+        if lines_to_send != '':
+            lines_send.append(lines_to_send)
+        lines_to_send = line + '\n'
+    else:
+        lines_to_send = lines_to_send + line + '\n'
+lines_send.append(lines_to_send)
+
+for line in lines_send:
+    line = "``` \n {} ``` \n".format(line)
+    client.send_message(chatid="@hamadatest", text=line, parse_mode='markdown')
