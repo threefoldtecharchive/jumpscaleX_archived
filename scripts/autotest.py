d7bff82ee19235f537e11c61dedd7bd7763918d8
ahmedhanafy725 <hanafya@codescalers.com>
2019-02-12 08:11:51 +0000

    add teardown for images and clean code

--- scripts/autotest.py
+++ scripts/autotest.py
@@ -3,24 +3,60 @@ from subprocess import run, PIPE
 from uuid import uuid4
 import sys
 
+chat_id = '@hamadatest'
 
-client = j.clients.telegram_bot.get("test")
-response = run('docker build --rm -t jumpscale ../', shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE)
-if response.returncode:
-    client.send_message(chatid="@hamadatest", text='Failed to bulid docker image')
-    sys.exit()
+def random_string():
+    str(uuid4()).replace('-', '')[:10]
 
-response = run('docker run --rm -t jumpscale /bin/bash -c "source /sandbox/env.sh; python3 /sandbox/code/github/threefoldtech/jumpscaleX/test.py"', shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE)
-if response.stderr not in [None, '']:
-    file_name = '{}.log'.format(str(uuid4()).replace('-', '')[:10])
-    with open (file_name, 'w+') as f:
-        f.write(response.stderr)
+def send_msg(msg):
+    client = j.clients.telegram_bot.get("test")
+    client.send_message(chatid=chat_id, text=msg)
 
-    file_link = '{}/{}'.format('serverip', file_name)
-    client.send_message(chatid="@hamadatest", text='test has errors ' + file_link)
-else:
-    client.send_message(chatid="@hamadatest", text='Tests Passed')
+def execute_cmd(cmd):
+    response = run(cmd, shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE)
+    return response
 
-response = run('docker rmi jumpscale', shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE)
-if response.returncode:
-    client.send_message(chatid="@hamadatest", text='Failed to remove the built docker image')
+def build_image():
+    response = execute_cmd('docker build --rm -t jumpscale /sandbox/code/github/threefoldtech/jumpscaleX')
+    if response.returncode:
+        send_msg('Failed to bulid docker image')
+        containers_remove()
+        images_clean()
+        sys.exit()
+
+def run_tests():
+    response = execute_cmd('docker run --rm -t jumpscale /bin/bash -c "source /sandbox/env.sh; python3 /sandbox/code/github/threefoldtech/jumpscaleX/test.py"')
+    if response.stderr not in [None, '']:
+        file_name = '{}.log'.format(random_string())
+        with open (file_name, 'w+') as f:
+            f.write(response.stderr)
+
+        file_link = '{}/{}'.format('serverip', file_name)
+        send_msg('test has errors ' + file_link)
+    else:
+        send_msg('Tests Passed')
+
+def containers_remove():
+    response = execute_cmd('docker ps -a | tail -n+2 | awk "{print\$1}"')
+    containers = response.stdout.split()
+    for container in containers:
+        execute_cmd('docker rm {}'.format(container))
+
+def images_clean():
+    response = execute_cmd('docker images | tail -n+2 | awk "{print \$1}"')
+    images = response.stdout.split()
+    response = execute_cmd('docker images | tail -n+2 | awk "{print \$3}"')
+    images_id = response.stdout.split()
+    for i in range(0, len(images)):
+        if images[i] == 'ubuntu':
+            continue
+        else:
+            response = execute_cmd('docker rm {}'.format(images_id[i]))
+
+if __name__ == "__main__":
+    build_image()
+    run_tests()
+    containers_remove()
+    images_clean()
+
+
