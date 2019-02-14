36c1173224c3cd47f4cfc3f4f83dea1c72233efa
ahmedhanafy725 <hanafya@codescalers.com>
2019-02-14 13:37:58 +0000

    edit autotest and test

--- test.py
+++ test.py
@@ -1,32 +1,6 @@
 from Jumpscale import j
 import traceback
 import sys
-# JSBASE = j.application.JSBaseClass
-# class DevelopmentTest(JSBASE):
-#     def __init__(self):
-#         JSBASE.__init__(self)
-#         self.path = ''
-
-#     def run_tests(self, path):
-#         self.path = path
-#         self._test_run(name='', obj_key='main')
-    
-#     @property
-#     def _dirpath(self):
-#         path = '/sandbox/code/github/threefoldtech/jumpscaleX/Jumpscale/'
-#         return path + self.path
-
-# if __name__ == "__main__":
-#     run = DevelopmentTest()
-#     run.run_tests(path='data/schema/')
-
-#     cla=j.servers.zdb.start_test_instance(destroydata=True,namespaces=["test"])
-#     cl = cla.namespace_get("test","1234")
-#     assert cla.ping()
-#     assert cl.ping()
-#     bcdb = j.data.bcdb.new("test", zdbclient=cl)
-#     bcdb.reset()
-#     run.run_tests(path='data/bcdb')
 
 try:
     j.clients.zdb.test()
