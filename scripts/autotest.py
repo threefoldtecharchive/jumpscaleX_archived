ffd6ba40cfae76990fccb9ced5b2740eb180c481
ahmedhanafy725 <hanafya@codescalers.com>
2019-02-24 15:19:34 +0200

    fix clean images

--- scripts/autotest.py
+++ scripts/autotest.py
@@ -1,6 +1,5 @@
 from Jumpscale import j
 from subprocess import run, PIPE
-from datetime import datetime
 from uuid import uuid4
 import sys
 import os 
@@ -15,17 +14,16 @@ def send_msg(msg):
     client.send_message(chatid=chat_id, text=msg)
 
 def execute_cmd(cmd):
-    rc, stdout, stderr = j.builder.tools.run(cmd)
-    return rc, stdout, stderr
+    response = run(cmd, shell=True, universal_newlines=True, stdout=PIPE, stderr=PIPE)
+    return response
 
 def build_image():
     image_name = random_string()
-    cmd = 'docker build --rm -t {} /sandbox/code/github/threefoldtech/jumpscaleX/scripts --build-arg commit={}'\
+    cmd = 'docker build --force-rm -t {} /sandbox/code/github/threefoldtech/jumpscaleX/scripts --build-arg commit={}'\
             .format(image_name, os.environ.get('commit'))
-    rc, _, _ = execute_cmd(cmd)
-    if rc:
+    response = execute_cmd(cmd)
+    if response.returncode:
         send_msg('Failed to install jumpscaleX')
-        images_clean(image_name)
         sys.exit()
     else:
         return image_name
@@ -35,32 +33,32 @@ def run_tests(image_name):
     env_cmd = "source /sandbox/env.sh; export NACL_SECRET={};".format(os.environ.get('Nacl'))
     run_cmd = "python3.6 /sandbox/code/github/threefoldtech/jumpscaleX/test.py 1>/dev/null"
     cmd = '{} "{} {}"'.format(docker_cmd, env_cmd, run_cmd)
-    _, stdout, _ = execute_cmd(cmd)
-    if 'Error In' in stdout:
-        file_name = '{}.log'.format(str(datetime.now())[:13].replace(' ', '_'))
+    response = execute_cmd(cmd)
+    if 'Error In' in response.stdout:
+        file_name = '{}.log'.format(os.environ.get('commit'))
         with open (file_name, 'w+') as f:
-            f.write(stdout[stdout.find('Error In'):])
+            f.write(response.stdout[response.stdout.find('Error In'):])
 
         file_link = '{}/{}'.format(os.environ.get('ServerIp'), file_name)
         send_msg('Tests had errors ' + file_link)
     else:
         send_msg('Tests Passed')
 
-# def containers_remove():
-#     response = execute_cmd('docker ps -a | tail -n+2 | awk "{print\$1}"')
-#     containers = response.stdout.split()
-#     for container in containers:
-#         response = execute_cmd('docker rm -f {}'.format(container))
-#         if response.returncode:
-#             send_msg('Failed to remove docker container')
-
-def images_clean(image_name):
-    rc, _, _ = execute_cmd('docker rmi -f {}'.format(image_name))
-    if rc:
-        send_msg('Failed to remove docker image')
-
+def images_clean(image_name=None):
+    if image_name:
+        response = execute_cmd('docker rmi -f {}'.format(image_name))
+        
+    # for case of failure to build docker images
+    response = execute_cmd('docker images | tail -n+2 | awk "{print \$5}"')
+    images_time = response.stdout.split()
+    response = execute_cmd('docker images | tail -n+2 | awk "{print \$3}"')
+    images_id = response.stdout.split()
+    if len(images_id) == len(images_time):
+        for i in range(0, len(images_id)):
+            if images_time[i] not in ['second', 'seconds', 'minutes', 'minute']:
+                response = execute_cmd('docker rmi -f {}'.format(images_id[i]))
+                
 if __name__ == "__main__":
     image_name = build_image()
     run_tests(image_name)
-    # containers_remove()
     images_clean(image_name)
