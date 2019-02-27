d67b0ce6b932108a8f5bfa3f76ab0ef7e7084001
BolaNasr <bola.nasr@live.com>
2019-02-27 18:36:13 +0200

    fix issue in cryptography install

--- install/InstallTools.py
+++ install/InstallTools.py
@@ -548,6 +548,7 @@ class Tools:
         if Tools._shell is None:
             script = """
             apt-get install -y ipython
+            apt-get install -y python-dev
             pip3 install ipython
             apt-get clean && apt-get update && apt-get install -y locales
             """
