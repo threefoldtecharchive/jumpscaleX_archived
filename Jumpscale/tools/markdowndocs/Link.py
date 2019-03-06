47f50c01be95f6b350fc39e151fc5a38cb11a8a0
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-03-06 17:00:24 +0200

    formatting and add comments to regular expressions

--- Jumpscale/tools/markdowndocs/Link.py
+++ Jumpscale/tools/markdowndocs/Link.py
@@ -1,6 +1,7 @@
 import toml
 import re
 import copy
+
 from urllib.parse import urlparse
 from Jumpscale import j
 
@@ -29,7 +30,19 @@ class CustomLink:
 
     URL_RE = re.compile(r'^(http|https)\:\/\/', re.IGNORECASE)
     REFERENCE_RE = re.compile(r'^\#(\d+)$')
-    REPO_PATH_RE = re.compile(r'^(?:(.*?)(?:\((.*?)\))?\:)?(.*?)$')
+    REPO_PATH_RE = re.compile(r'''
+        ^                   # start of the line/string
+        (?:                 # non-capturing group
+            (.*?)           # match any char -> repo
+            (?:             # non-capturing group
+                \(          # anything between (...)
+                    (.*?)   # in a group -> branch
+                \)
+            )?              # optional
+            \:              # match literal :
+        )?                  # optional
+        (.*?)$              # anything at the end after that -> path
+    ''', re.X)
 
     def __init__(self, link):
         self.link = link.strip()
@@ -114,7 +127,6 @@ class CustomLink:
         assert l.path == 'docs/test.md'
 
 
-
 class Linker:
     HOST = None
     ISSUE = None
@@ -167,34 +179,52 @@ class GithubLinker(Linker):
 
 
 class Link(j.application.JSBaseClass):
-    LINK_MARKDOWN_PATTERN = r'\!?\[([^\]\(]+)?\]\(([^\(\)]*?(\([^\(\)]*?\))?[^\(\)]*?)?\)'
-    LINK_MARKDOWN_RE = re.compile(LINK_MARKDOWN_PATTERN)
+    LINK_MARKDOWN_PATTERN = r'''
+        \!?                         # match optiona !
+        \[                          # start of descirption [
+            (
+                [^\]\(]+            # match anything other than ] or ( -> description text
+            )?                      # optional
+        \]                          # end of description
+        \(                          # start of link
+            (                       # -> full link text
+                [^\(\)]*?           # anything other than ( or )
+                (?:                 # possible one non-capturing group of (...) for the branch
+                    \(              # match literal (
+                        [^\(\)]*?   # match anything other than ( or )
+                    \)              # match literal )
+                )?
+                [^\(\)]*?           # anything other than ( or )
+            )??                     # optional
+        \)                          # end of link
+    '''
+    LINK_MARKDOWN_RE = re.compile(LINK_MARKDOWN_PATTERN, re.X)
 
-    def __init__(self,doc, source):
+    def __init__(self, doc, source):
         JSBASE.__init__(self)
         self.docsite = doc.docsite
         self.doc = doc
         self.extension = ""
-        self.source = source #original text
-        self.cat = ""  #category in image,doc,link,officedoc, imagelink  #doc is markdown
-        self.link_source = "" #text to replace when rewrite is needed
-        self.link_source_original = "" #original link
+        self.source = source  # original text
+        self.cat = ""  # category in image,doc,link,officedoc, imagelink  #doc is markdown
+        self.link_source = ""  # text to replace when rewrite is needed
+        self.link_source_original = ""  # original link
         self.error_msg = ""
         self.filename = ""
         self.filepath = ""
-        self.link_descr="" #whats inside the []
+        self.link_descr = ""  # whats inside the []
         self.link_to_doc = None
         self._process()
 
-    def _clean(self,name):
+    def _clean(self, name):
         return j.core.text.convert_to_snake_case(name)
 
-    def error(self,msg):
+    def error(self, msg):
         self.error_msg = msg
-        msg="**ERROR:** problem with link:%s\n%s"%(self.source,msg)
+        msg = "**ERROR:** problem with link:%s\n%s" % (self.source, msg)
         # self._log_error(msg)
         self.docsite.error_raise(msg, doc=self.doc)
-        self.doc._content = self.doc._content.replace(self.source,msg)
+        self.doc._content = self.doc._content.replace(self.source, msg)
         return msg
 
     def remove_quotes(self, s):
@@ -205,7 +235,7 @@ class Link(j.application.JSBaseClass):
     def parse_markdown(self, link_markdown):
         match = self.LINK_MARKDOWN_RE.match(link_markdown)
         if match:
-            match = match.groups()[0:2]
+            match = match.groups()
             return map(self.remove_quotes, match)
         raise ValueError('not a link markdown')
 
@@ -272,21 +302,20 @@ class Link(j.application.JSBaseClass):
 
     def _process(self):
         self.link_descr, self.link_source = self.parse_markdown(self.source)
-        if self.link_source.strip()=="":
-            return self.error("link is empty, but url is:%s"%self.source)
+        if self.link_source.strip() == "":
+            return self.error("link is empty, but url is:%s" % self.source)
 
         if "@" in self.link_descr:
-            self.link_source_original = self.link_descr.split("@")[1].strip() #was link to original source
+            self.link_source_original = self.link_descr.split("@")[1].strip()  # was link to original source
             self.link_descr = self.link_descr.split("@")[0].strip()
 
         custom_link = CustomLink(self.link_source)
         self.link_source = self.get_real_source(custom_link)
 
         if "?" in self.link_source:
-            lsource=self.link_source.split("?",1)[0]
+            lsource = self.link_source.split("?", 1)[0]
         else:
-            lsource =self.link_source
-
+            lsource = self.link_source
 
         self.extension = j.sal.fs.getFileExtension(lsource)
 
@@ -294,19 +323,19 @@ class Link(j.application.JSBaseClass):
             self.link_source_original = self.link_source
             if self.source.startswith("!"):
                 if "?" in self.link_source:
-                   link_source=self.link_source.split("?",1)[0]
+                    link_source = self.link_source.split("?", 1)[0]
                 else:
-                    link_source=self.link_source
+                    link_source = self.link_source
                 self.filename = self._clean(j.sal.fs.getBaseName(link_source))
 
-                if not self.extension in ["png", "jpg", "jpeg", "mov", "mp4","mp3","docx"]:
-                    self.extension = "jpeg" #to support url's like https://images.unsplash.com/photo-1533157961145-8cb586c448e1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4e252bcd55caa8958985866ad15ec954&auto=format&fit=crop&w=1534&q=80
+                if not self.extension in ["png", "jpg", "jpeg", "mov", "mp4", "mp3", "docx"]:
+                    self.extension = "jpeg"  # to support url's like https://images.unsplash.com/photo-1533157961145-8cb586c448e1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4e252bcd55caa8958985866ad15ec954&auto=format&fit=crop&w=1534&q=80
                     self.filename = self.filename + '.jpeg'
 
-                if j.sal.fs.getFileExtension(self.filename)!=self.extension:
+                if j.sal.fs.getFileExtension(self.filename) != self.extension:
                     j.shell()
             else:
-                #check link exists
+                # check link exists
                 self.cat = "link"
                 if self.docsite.links_verify:
                     self.link_verify()
@@ -316,8 +345,7 @@ class Link(j.application.JSBaseClass):
                     self.get_docsite(self.link_source, name=custom_link.repo)
                     self.filename = self._clean(Linker().join(custom_link.repo, custom_link.path))
                 else:
-                    self.filename = None #because is e.g. other site
-
+                    self.filename = None  # because is e.g. other site
 
         else:
             if self.link_source.strip() == "/":
@@ -328,33 +356,33 @@ class Link(j.application.JSBaseClass):
                 return
 
             if "?" in self.link_source:
-                self.link_source=self.link_source.split("?",1)[0]
+                self.link_source = self.link_source.split("?", 1)[0]
 
             if self.link_source.find("/") != -1 and self.extension != "md":
                 name = self.link_source.split("/")[-1]
             else:
                 name = self.link_source
 
-            self.filename = self._clean(name) #cleanly normalized name but extension still part of it
+            self.filename = self._clean(name)  # cleanly normalized name but extension still part of it
             #e.g. balance_inspiration_motivation_life_scene_wander_big.jpg
-            if self.filename.strip()=="":
+            if self.filename.strip() == "":
                 return self.error("filename is empty")
 
-            #only possible for images (video, image)
+            # only possible for images (video, image)
             if self.source.startswith("!"):
                 self.cat = "image"
                 if not self.extension in ["png", "jpg", "jpeg", "mov", "mp4"]:
-                    return self.error("found unsupported image file: extension:%s"%self.extension)
+                    return self.error("found unsupported image file: extension:%s" % self.extension)
 
-            if self.cat =="":
+            if self.cat == "":
                 if self.extension in ["png", "jpg", "jpeg", "mov", "mp4"]:
                     self.cat = "imagelink"
                 elif self.extension in ["doc", "docx", "pdf", "xls", "xlsx", "pdf"]:
                     self.cat = "officedoc"
-                elif self.extension in ["md","",None]:
-                    self.cat = "doc" #link to a markdown document
+                elif self.extension in ["md", "", None]:
+                    self.cat = "doc"  # link to a markdown document
                     try:
-                        self.link_to_doc =  self.docsite.doc_get(self.link_source ,die=True)
+                        self.link_to_doc = self.docsite.doc_get(self.link_source, die=True)
                     except Exception as e:
                         if "Cannot find doc" in str(e):
                             return self.error(str(e))
@@ -368,45 +396,46 @@ class Link(j.application.JSBaseClass):
     @property
     def markdown(self):
         if self.source.startswith("!"):
-            c="!"
+            c = "!"
             if self.link_source_original is not "":
-                descr="%s@%s"%(self.link_descr,self.link_source_original )
+                descr = "%s@%s" % (self.link_descr, self.link_source_original)
             else:
-                descr=self.link_descr
+                descr = self.link_descr
         else:
-            c=""
-            descr=self.link_descr
-        c+= "[%s](%s)"%(descr,self.link_source)
+            c = ""
+            descr = self.link_descr
+        c += "[%s](%s)" % (descr, self.link_source)
         return c
 
-    def download(self,dest):
+    def download(self, dest):
         if not "http" in self.link_source:
             return
-        self._log_info("download:%s\n%s"%(self.link_source_original,dest))
+        self._log_info("download:%s\n%s" % (self.link_source_original, dest))
         ddir = j.sal.fs.getDirName(dest)
         if not j.sal.fs.exists(dest):
-            cmd = "cd %s;rm -f %s;curl '%s' -o '%s'"%(ddir,dest,self.link_source_original,dest) #cannot use primitive something wrong sometimes with ssl verification
+            # cannot use primitive something wrong sometimes with ssl verification
+            cmd = "cd %s;rm -f %s;curl '%s' -o '%s'" % (ddir, dest, self.link_source_original, dest)
             j.sal.process.execute(cmd, die=False)
 
     def link_verify(self):
         def do():
             if any(link in self.link_source for link in SKIPPED_LINKS):
                 return True
-            self._log_info("check link exists:%s"%self.link_source)
+            self._log_info("check link exists:%s" % self.link_source)
             if not j.clients.http.ping(self.link_source_original):
-                self.error("link not alive:%s"%self.link_source_original)
+                self.error("link not alive:%s" % self.link_source_original)
                 return False
             return True
-        res =  self._cache.get(self.link_source, method=do, expire=3600)
+        res = self._cache.get(self.link_source, method=do, expire=3600)
         if res is not True:
             self.error(res)
             return False
 
-    def replace_in_txt(self,txt):
-        if len(self.source)<4:
+    def replace_in_txt(self, txt):
+        if len(self.source) < 4:
             j.shell()
             raise RuntimeError("something wrong with original source")
-        txt = txt.replace(self.source,self.markdown)
+        txt = txt.replace(self.source, self.markdown)
         # self.source = self.markdown #there is a new source now
         return txt
 
@@ -417,4 +446,3 @@ class Link(j.application.JSBaseClass):
             return "link:%s:%s" % (self.cat, self.filename)
 
     __str__ = __repr__
-
