4545490849e488288b4321ed105f18406f907343
Islam Taha <islamtaha2012@gmail.com>
2019-03-12 15:45:21 +0200

    Update 0-Test.sh

--- 0-Test.sh
+++ 0-Test.sh
@@ -5,4 +5,4 @@ source /sandbox/env.sh; js_shell 'j.servers.zdb.test()'
 source /sandbox/env.sh; js_shell 'j.clients.sshagent.test()'
 source /sandbox/env.sh; js_shell 'j.clients.sshkey.test()'
 source /sandbox/env.sh; pytest -v /sandbox/code/github/threefoldtech/jumpscaleX
-source /sandbox/env.sh; js_init generate; cd /sandbox/code/github/threefoldtech/jumpscaleX/Jumpscale/builder/test/; nosetests-3.4 -vs --logging-level=WARNING test_cases.py --tc-file=config.ini --tc=itsyou.username:$IYO_USERNAME --tc=itsyou.client_id:$IYO_CLIENT_ID --tc=itsyou.client_secret:$IYO_CLIENT_SECRET --tc=zos_node.node_ip:$ZOS_NODE_IP
+#source /sandbox/env.sh; js_init generate; cd /sandbox/code/github/threefoldtech/jumpscaleX/Jumpscale/builder/test/; nosetests-3.4 -vs --logging-level=WARNING test_cases.py --tc-file=config.ini --tc=itsyou.username:$IYO_USERNAME --tc=itsyou.client_id:$IYO_CLIENT_ID --tc=itsyou.client_secret:$IYO_CLIENT_SECRET --tc=zos_node.node_ip:$ZOS_NODE_IP
