ed0502a73668af41ec47d9413a3f9a9b2b67e811
Ahmed T. Youssef <xmonader@gmail.com>
2019-03-20 09:34:04 +0200

    update with resize param for googleslides tool

--- Jumpscale/tools/googleslides/GoogleSlides.py
+++ Jumpscale/tools/googleslides/GoogleSlides.py
@@ -14,69 +14,82 @@ class GoogleSlides(j.application.JSBaseClass):
         self.__imports__ = "Pillow"
         JSBASE.__init__(self)
 
-    def export(self, slideid, credfile, serviceaccount=False, background=None, websitedir="", indexfile="", themefile="", imagesize="MEDIUM"):
-        """export presentation to a reveal website directory.
+    def export(self, slideid, credfile, serviceaccount=False, background=None, websitedir="", indexfile="", themefile="", imagesize="MEDIUM", resize=None):
+            """export presentation to a reveal website directory.
 
-        Arguments:
-            slideid {[str} -- [presentation id or presentation full url]
-            credfile {[str]} -- [service account credentials or oauth2 credentials file path]
+            Arguments:
+                slideid {[str} -- [presentation id or presentation full url]
+                credfile {[str]} -- [service account credentials or oauth2 credentials file path]
 
-        Keyword Arguments:
-            serviceaccount {bool} -- [indicator for service account type credentials] (default: {False})
-            background {[str]} -- [background slide full url] (default: {None})
-            websitedir {str} -- [reveal website directory path] (default: {""})
-            indexfile {str} -- [index file path] (default: {""})
-            themefile {str} -- [special theme file path] (default: {""})
-            imagesize {str} -- [control the exported images size medium or large] (default: {"MEDIUM"})
+            Keyword Arguments:
+                serviceaccount {bool} -- [indicator for service account type credentials] (default: {False})
+                background {[str]} -- [background slide full url] (default: {None})
+                websitedir {str} -- [reveal website directory path] (default: {""})
+                indexfile {str} -- [index file path] (default: {""})
+                themefile {str} -- [special theme file path] (default: {""})
+                imagesize {str} -- [control the exported images size medium or large] (default: {"MEDIUM"})
 
-        Raises:
-            ValueError -- [Invalid presentation id.]
-            ValueError -- [Invalid credentials file.]
-        """
+            Raises:
+                ValueError -- [Invalid presentation id.]
+                ValueError -- [Invalid credentials file.]
+            """
 
-        from Jumpscale.tools.googleslides.slides2html.google_links_utils import get_slide_id, get_presentation_id, link_info
-        from Jumpscale.tools.googleslides.slides2html.image_utils import images_to_transparent_background, set_background_for_images
-        from Jumpscale.tools.googleslides.slides2html.generator import Generator
-        from Jumpscale.tools.googleslides.slides2html.downloader import Downloader
-        from Jumpscale.tools.googleslides.slides2html.revealjstemplate import BASIC_TEMPLATE
-        from Jumpscale.tools.googleslides.slides2html.tool import Tool
+            from Jumpscale.tools.googleslides.slides2html.google_links_utils import get_slide_id, get_presentation_id, link_info
+            from Jumpscale.tools.googleslides.slides2html.image_utils import images_to_transparent_background, set_background_for_images, resize_images
+            from Jumpscale.tools.googleslides.slides2html.generator import Generator
+            from Jumpscale.tools.googleslides.slides2html.downloader import Downloader
+            from Jumpscale.tools.googleslides.slides2html.revealjstemplate import BASIC_TEMPLATE
+            from Jumpscale.tools.googleslides.slides2html.tool import Tool
 
-        presentation_id = slideid
-        try:
-            presentation_id, slideid = link_info(slideid)
-        except ValueError:  # not a url, people using id as in old version.
-            pass
-        imagesize = imagesize.upper()
-        if imagesize not in ["MEDIUM", "LARGE"]:
-            raise ValueError(
-                "Invalid image size should be MEDIUM or LARGE")
-        if not indexfile:
-            indexfilepath = j.sal.fs.joinPaths(
-                websitedir, "{}.html".format(presentation_id))
-        else:
-            indexfilepath = j.sal.fs.joinPaths(
-                websitedir, "{}.html".format(indexfile))
+            presentation_id = slideid
+            try:
+                presentation_id, slideid = link_info(slideid)
+            except ValueError:  # not a url, people using id as in old version.
+                pass
+            imagesize = imagesize.upper()
+            if imagesize not in ["MEDIUM", "LARGE"]:
+                raise ValueError(
+                    "Invalid image size should be MEDIUM or LARGE")
 
-        destdir = j.sal.fs.joinPaths(websitedir, presentation_id)
-        credfile = os.path.abspath(os.path.expanduser(credfile))
-        if not os.path.exists(credfile):
-            raise ValueError(
-                "Invalid credential file: {}".format(credfile))
+            if resize and "," in resize:
+                try:
+                    newwidth, newheight = map(lambda x: int(x.strip()), resize.split(","))
+                except:
+                    raise ValueError("invalid size for --resize {}: should be 'width,height' ".format(resize))
 
-        theme = ""
-        themefilepath = os.path.expanduser(themefile)
-        if os.path.exists(themefilepath):
-            with open(themefilepath) as f:
-                theme = f.read()
-        else:
-            theme = BASIC_TEMPLATE
 
-        p2h = Tool(presentation_id, credfile,
-                    serviceaccount=serviceaccount)
-        p2h.downloader.thumbnailsize = imagesize
-        p2h.build_revealjs_site(destdir, indexfilepath, template=theme)
+            if not indexfile:
+                indexfilepath = j.sal.fs.joinPaths(
+                    websitedir, "{}.html".format(presentation_id))
+            else:
+                indexfilepath = j.sal.fs.joinPaths(
+                    websitedir, "{}.html".format(indexfile))
 
-        if background is not None:
-            bgpath = p2h.downloader.get_background(background, destdir)
-            p2h.convert_to_transparent_background(destdir)
-            p2h.set_images_background(destdir, bgpath)
+            destdir = j.sal.fs.joinPaths(websitedir, presentation_id)
+            credfile = os.path.abspath(os.path.expanduser(credfile))
+            if not os.path.exists(credfile):
+                raise ValueError(
+                    "Invalid credential file: {}".format(credfile))
+
+            theme = ""
+            themefilepath = os.path.expanduser(themefile)
+            if os.path.exists(themefilepath):
+                with open(themefilepath) as f:
+                    theme = f.read()
+            else:
+                theme = BASIC_TEMPLATE
+
+            p2h = Tool(presentation_id, credfile,
+                        serviceaccount=serviceaccount)
+            p2h.downloader.thumbnailsize = imagesize
+            p2h.build_revealjs_site(destdir, indexfilepath, template=theme)
+
+            if background is not None:
+                bgpath = p2h.downloader.get_background(background, destdir)
+                p2h.convert_to_transparent_background(destdir)
+                p2h.set_images_background(destdir, bgpath)
+
+
+            if resize:
+                newwidth, newheight = resize
+                resize_images(destdir, (newwidth, newheight))
--- Jumpscale/tools/googleslides/slides2html/tool.py
+++ Jumpscale/tools/googleslides/slides2html/tool.py
@@ -8,7 +8,7 @@ from oauth2client import file, client, tools
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from Jumpscale.tools.googleslides.slides2html.google_links_utils import get_slide_id, get_presentation_id, link_info
-from Jumpscale.tools.googleslides.slides2html.image_utils import images_to_transparent_background, set_background_for_images
+from Jumpscale.tools.googleslides.slides2html.image_utils import images_to_transparent_background, set_background_for_images, resize_images
 from Jumpscale.tools.googleslides.slides2html.generator import Generator
 from Jumpscale.tools.googleslides.slides2html.downloader import Downloader
 from Jumpscale.tools.googleslides.slides2html.revealjstemplate import BASIC_TEMPLATE
@@ -173,3 +173,132 @@ def cli(website, id, indexfile="", imagesize="medium", credfile="credentials.jso
         bgpath = p2h.downloader.get_background(background, destdir)
         p2h.convert_to_transparent_background(destdir)
         p2h.set_images_background(destdir, bgpath)
+
+
+class Tool:
+    def __init__(self, presentation_id, credfile="credentials.json", serviceaccount=False):
+        """Initialize slides2html tool.
+
+        Arguments:
+            presentation_id {[str]} -- presentation id (e.g "147sFqkzjr_caJrh5f4ZpRRdD0SZP32aGSBkfDNH31PM or full url.")
+
+        Keyword Arguments:
+            credfile {str} -- [description] (default: {"credentials.json"})
+
+        Raises:
+            RuntimeError -- [In case of invalid credential files.]
+
+        """
+        self.presentation_id = presentation_id
+        SCOPES = ['https://www.googleapis.com/auth/drive']
+
+        if not os.path.exists(credfile):
+            raise RuntimeError(
+                "please provide valid credentials.json file. https://console.developers.google.com/apis/credentials")
+
+        self.credfile = os.path.expanduser(credfile)
+        print("credfile: ", self.credfile)
+
+        credentials = None
+        service = None
+        if serviceaccount:
+            credentials = service_account.Credentials.from_service_account_file(
+                self.credfile, scopes=SCOPES)
+            service = build('slides', 'v1', credentials=credentials)
+        else:
+            userdir = os.path.expanduser("~")
+            tokenjson = os.path.join(userdir, ".token.json")
+            store = file.Storage(tokenjson)
+            credentials = store.get()
+            self.credfile = os.path.expanduser(credfile)
+            if not credentials or credentials.invalid:
+                flow = client.flow_from_clientsecrets(self.credfile, SCOPES)
+                credentials = tools.run_flow(flow, store)
+
+            service = build('slides', 'v1', http=credentials.authorize(Http()))
+
+        self.downloader = Downloader(presentation_id, service)
+        self.generator = Generator(presentation_id)
+
+    def build_revealjs_site(self, destdir="", entryfile="", presentation_dir="", template=BASIC_TEMPLATE):
+        """Build reveal.js based website.
+
+        Keyword Arguments:
+            destdir {str} -- directory under reveal.js website directory (default: {""})
+            entryfile {str} -- index file name (default: presentation id)
+            presentation_dir {str} -- directory to save images (default: {""})
+            template {[str]} -- [reveal.js template] (default: {BASIC_TEMPLATE})
+        """
+        self.downloader.download(destdir)
+        slides_infos = get_slides_info(destdir)
+        html = self.generator.generate_html(
+            slides_infos, revealjs_template=template)
+        if not entryfile:
+            entryfile = self.presentation_id
+
+        with open(entryfile, "w") as f:
+            f.write(html)
+
+    def convert_to_transparent_background(self, destdir):
+        images_to_transparent_background(destdir)
+
+    def set_images_background(self, destdir, bgpath):
+        set_background_for_images(destdir, bgpath)
+
+
+@click.command()
+@click.option("--website", help="Reveal.js site directory", required=True)
+@click.option("--id", help="presentation url or id", required=True)
+@click.option("--indexfile", help="index filename. will default to presentation id if not provided.", required=False)
+@click.option("--imagesize", help="image size (MEDIUM, LARGE)", default="medium", required=False)
+@click.option("--credfile", help="credentials file path", default="credentials.json", required=False)
+@click.option("--themefile", help="use your own reveal.js theme", default="", required=False)
+@click.option("--serviceaccount", help="use service account instead of normal oauth flow", default=False, is_flag=True, required=False)
+@click.option("--background", help="background image to be used for all of the slides", required=False)
+@click.option("--resize", help="resize image of (width,height)", required=False)
+def cli(website, id, indexfile="", imagesize="medium", credfile="credentials.json", themefile="", serviceaccount=False, background=None, resize=None):
+    presentation_id = id
+    try:
+        presentation_id, slide_id = link_info(id)
+    except ValueError:  # not a url, people using id as in old version.
+        pass
+    imagesize = imagesize.upper()
+    if imagesize not in ["MEDIUM", "LARGE"]:
+        raise ValueError("Invalid image size should be MEDIUM or LARGE")
+
+    if resize and "," in resize:
+        try:
+            newwidth, newheight = map(lambda x: int(x.strip()), resize.split(","))
+        except:
+            raise ValueError("invalid size for --resize {}: should be 'width,height' ".format(resize))
+
+    if not indexfile:
+        indexfilepath = os.path.join(website, "{}.html".format(presentation_id))
+    else:
+        indexfilepath = os.path.join(website, "{}.html".format(indexfile))
+
+    destdir = os.path.join(website, presentation_id)
+    credfile = os.path.abspath(os.path.expanduser(credfile))
+    if not os.path.exists(credfile):
+        raise ValueError("Invalid credential file: {}".format(credfile))
+
+    theme = ""
+    themefilepath = os.path.expanduser(themefile)
+    if os.path.exists(themefilepath):
+        with open(themefilepath) as f:
+            theme = f.read()
+    else:
+        theme = BASIC_TEMPLATE
+
+    p2h = Tool(presentation_id, credfile, serviceaccount=serviceaccount)
+    p2h.downloader.thumbnailsize = imagesize
+    p2h.build_revealjs_site(destdir, indexfilepath, template=theme)
+
+    if background is not None:
+        bgpath = p2h.downloader.get_background(background, destdir)
+        p2h.convert_to_transparent_background(destdir)
+        p2h.set_images_background(destdir, bgpath)
+
+    if resize and "," in resize:
+        newwidth, newheight = map(lambda x: int(x.strip()), resize.split(","))
+        resize_images(destdir, (newwidth, newheight))
