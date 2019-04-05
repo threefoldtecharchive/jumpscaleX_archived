46e35d0b6d910bc2434d6cf51fc1e9df4ee74345
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-04-05 17:08:57 +0200

    zos client: fix updating internal client data

--- Jumpscale/clients/zero_os/ZeroOSClient.py
+++ Jumpscale/clients/zero_os/ZeroOSClient.py
@@ -3,6 +3,7 @@ from Jumpscale.sal_zos.node.Node import Node
 
 from .protocol.Client import Client as ProtocolClient
 
+
 class ZeroOSClient(j.application.JSBaseConfigClass, Node):
 
     _SCHEMATEXT = """
@@ -19,17 +20,14 @@ class ZeroOSClient(j.application.JSBaseConfigClass, Node):
 
     def _init(self):
         client = ProtocolClient(host=self.host, port=self.port,
-            unixsocket=self.unixsocket,
-            password=self.password, db=self.db,
-            ssl=self.ssl, timeout=self.timeout)
+                                unixsocket=self.unixsocket,
+                                password=self.password, db=self.db,
+                                ssl=self.ssl, timeout=self.timeout)
         Node.__init__(self, client=client)
 
     def _update_trigger(self, key, value):
         try:
-            data = {}
-            data.update(self.data._ddict)
-            data[key] = value
-            self.client.data._data_update(data=data)
+            setattr(self.client, key, value)
         except AttributeError:
-            # not yet initializaed
+            # not a client attribute
             pass
