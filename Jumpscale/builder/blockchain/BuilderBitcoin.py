5b0c7500a3ff6fcd53778136e462184cde813a5b
Waleed Hammam <waleed.hammam@gmail.com>
2019-04-09 18:08:00 +0200

    fix bitcoin builder and build from source code

--- Jumpscale/builder/blockchain/BuilderBitcoin.py
+++ Jumpscale/builder/blockchain/BuilderBitcoin.py
@@ -1,47 +1,109 @@
 from Jumpscale import j
 
-
+builder_method = j.builder.system.builder_method
 
 class BuilderBitcoin(j.builder.system._BaseClass):
     NAME = "bitcoind"
 
     def _init(self):
-        self.BITCOIN_64BIT_URL = "https://bitcoin.org/bin/bitcoin-core-0.16.0/bitcoin-0.16.0-x86_64-linux-gnu.tar.gz"
-        self.DOWNLOAD_DEST = j.core.tools.text_replace("{DIR_VAR}/build/bitcoin-0.16.0.tar.gz")
-        self.EXTRACTED_FILEPATH = j.core.tools.text_replace("{DIR_TEMP}/bitcoin-0.16.0")
+        self.DIR_BUILD = self._replace("{DIR_VAR}/build/bitcoin")
+        j.sal.fs.createDir(self.DIR_BUILD)
 
+    @builder_method()
+    def build(self): 
 
-    def build(self, reset=False):
-        """Get/Build the binaries of bitcoin
-        Keyword Arguments:
-            reset {bool} -- reset the build process (default: {False})
+        j.builder.buildenv.install()
+        self.profile_builder_select()
+
+        # dependancies
+        if self.tools.isUbuntu:
+            j.builder.system.package.ensure('g++')
+            self.system.package.mdupdate()
+            self.system.package.install([
+                "build-essential", "libtool", "autotools-dev", "pkg-config",
+                "libboost-all-dev", "libqt5gui5", "libqt5core5a", "libqt5dbus5",
+                "qttools5-dev", "qttools5-dev-tools", "libprotobuf-dev", "protobuf-compiler",
+                "libqrencode-dev", "autoconf", "openssl", "libssl-dev", "libevent-dev", 
+                "libminiupnpc-dev", "bsdmainutils"
+            ])
+
+        '''
+        ' splitting build into 3 steps:
+        ' - clone bitcoin source code -v 0.18
+        ' - build & link berkly db libs with source code to activate wallet
+        ' - executing installation scripts
+        '''
+        clone_bitcoin_script = """
+                    cd {DIR_BUILD}
+                    git clone https://github.com/bitcoin/bitcoin.git --branch 0.18
+                    """
+        self._execute(clone_bitcoin_script)
+
+        # berkely db v4.8
+        berkly_config = """
+            cd {DIR_BUILD}
+            wget 'http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz'
+            tar -xzvf db-4.8.30.NC.tar.gz
+            cd {DIR_BUILD}/db-4.8.30.NC/build_unix/
+            ../dist/configure --enable-cxx --disable-shared --with-pic --prefix={DIR_BUILD}/bitcoin
+            make install
+            """
+        self._execute(berkly_config)
+
+        # tag v0.18
+        script = """
+        cd {DIR_BUILD}/bitcoin
+        ./autogen.sh
+        ./configure LDFLAGS="-L{DIR_BUILD}/bitcoin/lib/" CPPFLAGS="-I{DIR_BUILD}/bitcoin/include/"
+        make
+        make install
         """
+        self._execute(script)
 
-        if self._done_get('build') and reset is False:
-            return
+    @builder_method()
+    def install(self):
+        # bitcoin bins
+        self._copy('{DIR_BUILD}/bitcoin/src/bitcoind', '{DIR_BIN}') 
+        self._copy('{DIR_BUILD}/bitcoin/src/bitcoin-cli', '{DIR_BIN}') 
+        self._copy('{DIR_BUILD}/bitcoin/src/bitcoin-wallet', '{DIR_BIN}')
+        self._copy('{DIR_BUILD}/bitcoin/src/bitcoin-tx', '{DIR_BIN}') 
 
-        if not j.builder.tools.file_exists(self.DOWNLOAD_DEST):
-            j.builder.tools.file_download(self.BITCOIN_64BIT_URL, self.DOWNLOAD_DEST)
+    @builder_method()
+    def sandbox(self, zhub_client=None):
+        self.profile_sandbox_select()
+        self._copy('{DIR_BIN}/bitcoind', '{DIR_SANDBOX}') 
+        self._copy('{DIR_BIN}/bitcoin-cli', '{DIR_SANDBOX}') 
+        self._copy('{DIR_BIN}/bitcoin-wallet', '{DIR_SANDBOX}')
+        self._copy('{DIR_BIN}/bitcoin-tx', '{DIR_SANDBOX}') 
+    
+    @property
+    def startup_cmds(self):
+        # bitcoin daemon
+        cmd = j.tools.startupcmd.get(self.NAME, cmd='bitcoind')
+        return [cmd]
 
-        j.builder.tools.file_expand(self.DOWNLOAD_DEST, "{DIR_TEMP}")
+    @builder_method()
+    def stop(self):
+        # killing the daemon
+        j.sal.process.killProcessByName(self.NAME)
 
-        self._done_set('build')
+    @builder_method()
+    def clean(self):
+        self._remove(self.DIR_BUILD)
+        self._remove(self.DIR_SANDBOX)
 
+    '''
+    testing running daemon
+    add tests to bitcoin
+    ''' 
+    @builder_method()
+    def test(self):
+        
+        if self.running():
+            self.stop()
 
-    def install(self, reset=False):
-        """
-        Install the bitcoind binaries
-        """
+        self.start()
+        assert self.running()
 
-        if self._done_get('install') and reset is False:
-            return
-
-        self.build(reset=reset)
-
-        cmd = j.core.tools.text_replace('cp {}/bin/* {DIR_BIN}/'.format(self.EXTRACTED_FILEPATH))
-        j.sal.process.execute(cmd)
-
-        cmd = j.core.tools.text_replace('cp {}/lib/* $LIBDIR/'.format(self.EXTRACTED_FILEPATH))
-        j.sal.process.execute(cmd)
-
-        self._done_set('install')
+                    
+        self._log_info("TEST SUCCESS: Bitcoin daemon is running")  
