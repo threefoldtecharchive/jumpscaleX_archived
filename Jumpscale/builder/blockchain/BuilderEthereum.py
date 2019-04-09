88ad80f19e77ce4713675f39ff3b8b4b8b1f05d5
Rana Tarek Hassan <rana.t.hassan@gmail.com>
2019-04-09 12:46:33 +0000

    Fix Ethereum builder and add test #299

--- Jumpscale/builder/blockchain/BuilderEthereum.py
+++ Jumpscale/builder/blockchain/BuilderEthereum.py
@@ -1,5 +1,7 @@
 from Jumpscale import j
 
+builder_method = j.builder.system.builder_method
+
 class BuilderEthereum(j.builder.system._BaseClass):
     NAME = "geth"
 
@@ -7,39 +9,29 @@ class BuilderEthereum(j.builder.system._BaseClass):
         self.geth_repo = "github.com/ethereum/go-ethereum"
         self.package_path = j.builder.runtimes.golang.package_path_get("ethereum/go-ethereum")
 
-
+    @builder_method()
     def build(self, reset=False):
         """Build the binaries of ethereum
         Keyword Arguments:
             reset {bool} -- reset the build process (default: {False})
         """
-
-        if self._done_get('build') and reset is False:
-            return
-
-        if not j.builder.runtimes.golang.is_installed:
-            j.builder.runtimes.golang.install()
+        j.builder.runtimes.golang.install()
 
         j.builder.runtimes.golang.get(self.geth_repo)
 
-        j.builder.runtimes.golang.execute("cd {} && go build -a -ldflags '-w -extldflags -static' ./cmd/geth".format(self.package_path))
-        j.builder.runtimes.golang.execute("cd {} && go build -a -ldflags '-w -extldflags -static' ./cmd/bootnode".format(self.package_path))
+        self._execute("cd {} && go build -a -ldflags '-w -extldflags -static' ./cmd/geth".format(self.package_path))
+        self._execute("cd {} && go build -a -ldflags '-w -extldflags -static' ./cmd/bootnode".format(self.package_path))
 
-        self._done_set('build')
-
+    @builder_method()
     def install(self, reset=False):
         """
         Install the binaries of ethereum
         """
-        if self._done_get('install') and reset is False:
-            return
+        bin_path = self.tools.joinpaths(self.package_path, self.NAME)
+        self.tools.dir_ensure('{DIR_BIN}')
+        self.tools.file_copy(bin_path, self._replace('{DIR_BIN}'))
 
-        self.build(reset=reset)
-
-        bin_path = self.tools.joinpaths(self.package_path, "geth")
-        self.tools.file_copy(bin_path, "/sandbox/bin")
-        self._done_set('install')
-
+    @builder_method()
     def stop(self, pid=None, sig=None):
         """Stops geth process
 
@@ -53,29 +45,50 @@ class BuilderEthereum(j.builder.system._BaseClass):
         else:
             j.sal.process.killProcessByName(self.NAME, sig)
 
-    def sandbox(self, sandbox_dir="/tmp/builder/ethereum", flist=True, hub_instance=None, reset=False):
-        """
-        sandbbox go-ethereum
-        :param sandbox_dir: the directory to sandbox to (default: {"/tmp/builder/ethereum"})
-        :param create_flist: if True and flist will be created after sandboxing (default: {False})
-        :param hub_instance: zhub_client instance which will be used to uplload the flist (default: {None})
-        :param reset: reset the process (default: {False})
-        """
-        if self._done_check('sandbox') and not reset:
-            return
-        self.build(reset = reset)
+    @property
+    def startup_cmds(self):
 
-        bin_path = self.tools.joinpaths(self.package_path, "geth")
-        bin_dest = self.tools.joinpaths(sandbox_dir, 'sandbox', 'bin')
+        cmd = j.tools.startupcmd.get("geth", cmd='geth')
+        return [cmd]
+
+    @builder_method()
+    def sandbox(self, zhub_client=None,flist_create=True):
+        """
+        sandbox go-ethereum
+        Copy built bins and config files to sandbox specific directory and create flist and upload it to the hub if flist_create is True
+            :param zhub_client: hub instance to upload flist to
+            :type zhub_client:str
+            :param flist_create: create flist after copying files
+            :type flist_create:bool
+        """
+        dir_dest = j.sal.fs.joinPaths(
+            "/sandbox/var/build", "{}/sandbox".format(self.DIR_SANDBOX)
+        )
+        bin_path = self.tools.joinpaths(self._replace("{DIR_BIN}"), self.NAME)
+        bin_dest = self.tools.joinpaths(dir_dest , "bin" , self.NAME)
         self.tools.dir_ensure(bin_dest)
         self.tools.file_copy(bin_path, bin_dest)
         
         bootnode_bin_path = self.tools.joinpaths(self.package_path, "bootnode")
-        bootnode_bin_dest = self.tools.joinpaths(sandbox_dir, 'sandbox', 'bin')
+        bootnode_bin_dest = self.tools.joinpaths(dir_dest , "bin" ,self.NAME)
         self.tools.dir_ensure(bootnode_bin_dest)
         self.tools.file_copy(bootnode_bin_path, bootnode_bin_dest)
 
-        if flist:
-            print(self.flist_create(sandbox_dir=sandbox_dir, hub_instance=hub_instance))
-        self._done_set('sandbox')
+    def test(self):
+        '''Tests the builder by performing the following:
+        build(), install(), start()-> geth running, stop()-> geth not running    
+        '''
+        if self.running():
+            self.stop()
+        self.build(reset=True)
+        self.install()
+
+        assert not self.running()
+        # check start is working
+        self.start()
+        assert self.running()
+        # check stop is working
+        self.stop()
+        assert not self.running()
+        self._log_info("Ethereum test successfull")    
 
