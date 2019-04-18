c682efde5c3eef18216ab8710269da9ba395d014
andrewayoub <andrew.a.mikhaeel@gmail.com>
2019-04-18 16:20:14 +0200

    use caching in gdrive client

--- Jumpscale/clients/gdrive/GDriveClient.py
+++ Jumpscale/clients/gdrive/GDriveClient.py
@@ -48,21 +48,33 @@ class GDriveClient(JSConfigClient):
         file = GDriveFile(self.service_get(service_name, service_version), id=file_id)
         return file
 
+    def exportFile(self, file_id, destpath="/tmp", service_name="drive", service_version="v3"):
+        def do():
+            file = self.getFile(file_id, service_name=service_name, service_version=service_version)
+            file.exportPDF(path=destpath)
+            return True
+
+        return self._cache.get("exportFile", method=do, expire=300)
+
     def exportSlides(self, presentation, destpath="/tmp", staticdir=None, size='MEDIUM'):
-        from Jumpscale.tools.googleslides.slides2html.downloader import Downloader
-        # presentation should be the guid 
-        # should extract the presentation if full path
-        os.makedirs(destpath, exist_ok=True)
-        service = self.service_get("slides", "v1")
-        downloader = Downloader(presentation, service, size)
-        downloader.download(destpath)
-        presentation_dir = j.sal.fs.joinPaths(destpath, presentation)
-        os.makedirs(presentation_dir, exist_ok=True)
-        slides = [x for x in os.listdir(destpath) if x.endswith(".png") and "_" in x and "background_" not in x]
-        for image in slides:
-            imagepath = j.sal.fs.joinPaths(destpath, image)
-            slideimage = image.split("_",maxsplit=1)[1]   # 00_asdsadasda.png remove the leading zeros and _
-            newimagepath = j.sal.fs.joinPaths(presentation_dir, slideimage)
-            j.sal.fs.moveFile(imagepath, newimagepath)
-        if staticdir:
-            j.sal.fs.moveDir(presentation_dir, staticdir)
+        def do():
+            from Jumpscale.tools.googleslides.slides2html.downloader import Downloader
+            # presentation should be the guid
+            # should extract the presentation if full path
+            os.makedirs(destpath, exist_ok=True)
+            service = self.service_get("slides", "v1")
+            downloader = Downloader(presentation, service, size)
+            downloader.download(destpath)
+            presentation_dir = j.sal.fs.joinPaths(destpath, presentation)
+            os.makedirs(presentation_dir, exist_ok=True)
+            slides = [x for x in os.listdir(destpath) if x.endswith(".png") and "_" in x and "background_" not in x]
+            for image in slides:
+                imagepath = j.sal.fs.joinPaths(destpath, image)
+                slideimage = image.split("_",maxsplit=1)[1]   # 00_asdsadasda.png remove the leading zeros and _
+                newimagepath = j.sal.fs.joinPaths(presentation_dir, slideimage)
+                j.sal.fs.moveFile(imagepath, newimagepath)
+            if staticdir:
+                j.sal.fs.moveDir(presentation_dir, staticdir)
+            return True
+
+        return self._cache.get("exportSlides", method=do, expire=300)
