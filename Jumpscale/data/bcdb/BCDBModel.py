9bf0fcf74ab215952cfc720f4b21103813ea01d2
serboctor <boctor.sarah@gmail.com>
2019-04-23 13:51:53 +0200

    reload cache the first time you use a model

--- Jumpscale/data/bcdb/BCDBModel.py
+++ Jumpscale/data/bcdb/BCDBModel.py
@@ -98,9 +98,9 @@ class BCDBModel(j.application.JSBaseClass):
 
         self.schema = self.bcdb.meta.schema_set(self.schema)
 
-        if not j.clients.credis_core.keys(self._redis_prefix + b"*"):
-            for model in self.get_all():
-                self._index_key_set("name", model.name, model.id)
+        # load all objects in redis
+        for obj in self.get_all():
+            self._index_key_set("name", obj.name, obj.id)
         self._init_index()  # goal is to be overruled by users
 
     def trigger_add(self, method):
