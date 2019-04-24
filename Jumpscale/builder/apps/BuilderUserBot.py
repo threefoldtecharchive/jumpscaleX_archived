7d0ed976b0f277fadae496dd5c12f02e416d7eed
serboctor <boctor.sarah@gmail.com>
2019-04-24 14:40:20 +0200

    Change the base flist for merging userbot flist

--- Jumpscale/builder/apps/BuilderUserBot.py
+++ Jumpscale/builder/apps/BuilderUserBot.py
@@ -40,7 +40,7 @@ class BuilderUserBot(j.builder.system._BaseClass):
         j.builder.db.zdb.install()
 
     @builder_method()
-    def sandbox(self, reset=False, zhub_client=None, flist_create=True, merge_base_flist="tf-autobuilder/threefoldtech-jumpscaleX-autostart-development.flist"):
+    def sandbox(self, reset=False, zhub_client=None, flist_create=True, merge_base_flist="tf-autobuilder/threefoldtech-jumpscaleX-development.flist "):
         j.builder.apps.digitalme.sandbox(reset=True)
         j.tools.sandboxer.copyTo(j.builder.apps.digitalme.DIR_SANDBOX, self.DIR_SANDBOX)
 
