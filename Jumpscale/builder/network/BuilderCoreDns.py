e958acbd30746af2f91302bc3ae5c5748060a6f8
Waleed Hammam <waleed.hammam@gmail.com>
2019-05-08 15:10:20 +0200

    fixed configurations in coredns builder

--- Jumpscale/builder/network/BuilderCoreDns.py
+++ Jumpscale/builder/network/BuilderCoreDns.py
@@ -25,6 +25,7 @@ class BuilderCoreDns(BuilderGolangTools, j.builder.system._BaseClass):
     def _init(self):
         super()._init()
         self.package_path = self.package_path_get(self.NAME)
+        self.templates_dir = self.tools.joinpaths(j.sal.fs.getDirName(__file__), 'templates')
 
     @builder_method()
     def build(self):
@@ -73,15 +74,25 @@ class BuilderCoreDns(BuilderGolangTools, j.builder.system._BaseClass):
 
     @builder_method()
     def sandbox(self, zhub_client=None, flist_create=False):
+        
+        # copy bins
         coredns_bin = j.sal.fs.joinPaths('{DIR_BIN}', self.NAME)
-        dir_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, 'sandbox')
-        self.tools.dir_ensure(dir_dest)
-        self._copy(coredns_bin, dir_dest)
+        bin_dir_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, 'sandbox', 'bin')
+        self.tools.dir_ensure(bin_dir_dest)
+        self._copy(coredns_bin, bin_dir_dest)
 
+        # config
+        config_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, 'sandbox', 'cfg')
+        self._copy('/sandbox/cfg/coredns.conf', config_dest)
+
+        # startup toml
+        startup_file = self.tools.joinpaths(self.templates_dir, 'coredns_startup.toml')
+        self._copy(startup_file, config_dest)
+
+        # add certs 
         dir_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, 'etc/ssl/certs/')
         self.tools.dir_ensure(dir_dest)
         self._copy('/etc/ssl/certs', dir_dest)
-        self._copy('/sandbox/cfg/coredns.conf', self.DIR_SANDBOX)
 
     @builder_method()
     def test(self):
@@ -113,12 +124,12 @@ class BuilderCoreDns(BuilderGolangTools, j.builder.system._BaseClass):
     @builder_method()
     def test_zos(self, zos_client="", zhub_client=""):
         self.sandbox(zhub_client=zhub_client, flist_create=True)
-        flist = "https://hub.grid.tf/{}/coredns.flist".format(zhub_client.name)
+        flist = "https://hub.grid.tf/{}/coredns.flist".format(zhub_client.username)
         test_container = zos_client.containers.create(name="test_coredns", flist=flist, ports={1053:1053}, host_network=True)
         client = test_container.client
         assert client.ping()
-        assert client.filesystem.list('/sandbox')[0]['name'] == 'coredns'
-        client.system('/sandbox/coredns -dns.port 1053')
+        assert client.filesystem.list('/sandbox/bin')[0]['name'] == 'coredns'
+        client.system('/sandbox/bin/coredns -dns.port 1053')
         assert test_container.is_port_listening(1053)
         for job in client.job.list(): 
             client.job.kill(job['cmd']['id']) 
