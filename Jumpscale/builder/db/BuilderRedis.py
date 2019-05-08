cd9ef5fb4387a4439a711cbf59d9a52ee2f318d6
0xislamtaha <islamtaha2012@gmail.com>
2019-05-08 15:21:26 +0200

    redis sandbox

--- Jumpscale/builder/db/BuilderRedis.py
+++ Jumpscale/builder/db/BuilderRedis.py
@@ -51,9 +51,16 @@ class BuilderRedis(j.builder.system._BaseClass):
     def sandbox(self, reset=False, zhub_client=None, flist_create=False):
         bin_dest = j.sal.fs.joinPaths("/sandbox/var/build", "{}/sandbox".format(self.DIR_SANDBOX))
         self.tools.dir_ensure(bin_dest)
-        redis_server_bin_path = self.tools.joinpaths("{DIR_BIN}", self.NAME)
-        redis_client_bin_path = self.tools.joinpaths("{DIR_BIN}", "redis-cli")
-        self.tools.file_copy(redis_server_bin_path, bin_dest)
-        self.tools.file_copy(redis_client_bin_path, bin_dest)
 
+        bins = ['redis-server', 'redis-cli']
+        for bin in bins:
+            bin_path = self.tools.joinpaths("{DIR_BIN}", bin)
+            self.tools.file_copy(bin_path, bin_dest)
+
+
+        lib_dest = self.tools.joinpaths(self.DIR_SANDBOX, 'sandbox/lib')
+        self.tools.dir_ensure(lib_dest)
+        for bin in bins:
+            dir_src = self.tools.joinpaths(j.core.dirs.BINDIR, bin)
+            j.tools.sandboxer.libs_sandbox(dir_src, lib_dest, exclude_sys_libs=False)
 
