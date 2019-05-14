5cf99882e61747afc860110b1502b5db5c89800e
0xislamtaha <islamtaha2012@gmail.com>
2019-05-14 16:04:48 +0200

    redis sandbox

--- Jumpscale/builder/db/BuilderRedis.py
+++ Jumpscale/builder/db/BuilderRedis.py
@@ -47,11 +47,30 @@ class BuilderRedis(j.builder.system._BaseClass):
         return cmds
   
     @builder_method()
-    def sandbox(self, reset=False, zhub_client=None, flist_create=False):
-        bins = ['redis-server', 'redis-cli']
+    def sandbox(self, reset=False, zhub_client=None, flist_create=False, merge_base_flist="tf-autobuilder/threefoldtech-jumpscaleX-development.flist"):
+        '''Copy built bins to dest_path and create flist if create_flist = True
 
-        lib_dest = self.tools.joinpaths(self.DIR_SANDBOX, 'sandbox')
+        :param dest_path: destination path to copy files into
+        :type dest_path: str
+        :param sandbox_dir: path to sandbox
+        :type sandbox_dir: str
+        :param create_flist: create flist after copying files
+        :type create_flist:bool
+        :param zhub_client: hub instance to upload flist tos
+        :type zhub_client:str
+        '''
+        dest_path = self.DIR_SANDBOX
+        j.builder.web.openresty.sandbox(reset=reset)
+
+        bins = ['redis-server', 'redis-cli']
+        for bin_name in bins:
+            dir_src = self.tools.joinpaths(j.core.dirs.BINDIR, bin_name)
+            dir_dest = self.tools.joinpaths(dest_path, j.core.dirs.BINDIR[1:])
+            self.tools.dir_ensure(dir_dest)
+            self._copy(dir_src, dir_dest)
+
+        lib_dest = self.tools.joinpaths(dest_path, 'sandbox/lib')
         self.tools.dir_ensure(lib_dest)
         for bin in bins:
-            bin_path = self.tools.joinpaths(j.core.dirs.BINDIR, bin)
-            j.tools.sandboxer.sandbox_chroot(bin_path, lib_dest)
+            dir_src = self.tools.joinpaths(j.core.dirs.BINDIR, bin)
+            j.tools.sandboxer.libs_sandbox(dir_src, lib_dest, exclude_sys_libs=False)
