e153dfc42ccc918cd1f882cf7744e27ead607beb
Waleed Hammam <waleed.hammam@gmail.com>
2019-05-15 16:06:06 +0200

    Refactor capnp builder - add sandbox #299

--- Jumpscale/builder/libs/BuilderCapnp.py
+++ Jumpscale/builder/libs/BuilderCapnp.py
@@ -5,7 +5,7 @@ JSBASE = j.builder.system._BaseClass
 builder_method = j.builder.system.builder_method
 
 class BuilderCapnp(JSBASE):
-    NAME = "capnp"
+    NAME = 'capnp'
 
     @builder_method()
     def build(self):
@@ -14,29 +14,27 @@ class BuilderCapnp(JSBASE):
 
         kosmos 'j.builder.libs.capnp.build(reset=True)'
         kosmos 'j.builder.libs.capnp.build()'
-
         """
 
-        # self.prefab.system.package.mdupdate()
-        j.builder.buildenv.install()
+        #j.builder.buildenv.install()
         if self.tools.isUbuntu:
             j.builder.system.package.ensure('g++')
 
-        # url = "https://capnproto.org/capnproto-c++-0.6.1.tar.gz"
-        url = "https://capnproto.org/capnproto-c++-0.7.0.tar.gz"
-        self.tools.file_download(url, to="{}/capnproto".format(self.DIR_BUILD), overwrite=False, retry=3,
-                                       expand=True, minsizekb=900, removeTopDir=True, deletedest=True)
+        # build tools
+        self.system.package.mdupdate()
+        self.system.package.install(['autoconf', 'automake', 'libtool'])
 
-        self.profile_builder_select()
-
-        script = """
-        cd {DIR_BUILD}/capnproto
-        ./configure CXXFLAGS="-DHOLES_NOT_SUPPORTED"
+        # build from source
+        install_cmd = '''
+        cd {DIR_BUILD}
+        git clone --depth 1 https://github.com/sandstorm-io/capnproto.git
+        cd capnproto/c++
+        autoreconf -i
+        ./configure
         make -j6 check
         make install
-        """
-        self._execute(script)
-
+        '''
+        self._execute(install_cmd, timeout=1000)
 
     @builder_method()
     def install(self):
@@ -47,14 +45,109 @@ class BuilderCapnp(JSBASE):
         """
         if self.tools.isUbuntu:
             j.builder.system.package.ensure('g++')
-        j.builder.runtimes.python.pip_package_install(['cython', 'setuptools', 'pycapnp'])
+        #j.builder.runtimes.python.pip_package_install(['cython', 'setuptools', 'pycapnp'])
+        bins = ['capnp', 'capnp-afl-testcase', 'capnpc-c++', 'capnp-test'
+               ,'capnpc-capnp', 'capnp-evolution-test']
 
+        libs = [
+            'libkj-http.so',
+            'libkj-async.so',
+            'libcapnpc.so',
+            'libkj-tls.so',
+            'libcapnp.so',
+            'libkj-test.so',
+            'libcapnp-json.so',
+            'libcapnp-rpc.so',
+            'libkj.so',
+            'libkj-async-0.8-dev.so',
+            'libkj-test-0.8-dev.so',
+            'libcapnpc-0.8-dev.so',
+            'libcapnp-json-0.8-dev.so',
+            'libcapnp-0.8-dev.so',
+            'libkj-0.8-dev.so',
+            'libkj-http-0.8-dev.so',
+            'libkj-tls-0.8-dev.so',
+            'libcapnp-rpc-0.8-dev.so'
+        ]
+        # copy bins
+        bins_src_path = '{DIR_BUILD}/capnproto/c++/.libs/'
+        for bin in bins:
+            self._copy(bins_src_path + bin, '{DIR_BIN}')
+        
+        # copy libs
+        for lib in libs:
+            self._copy(bins_src_path + lib, '/sandbox/lib')
+
+    @builder_method()
+    def clean(self):
+        self._remove(self.DIR_BUILD)
+    
+    @property
+    def startup_cmds(self):
+        cmd = '/sandbox/bin/capnp'
+        cmds = [j.tools.startupcmd.get(name='capnp', cmd=cmd)]
+        return cmds
+
+    @builder_method()
+    def reset(self):
+        super().reset()
+        self.clean()
+
+    @builder_method()
+    def stop(self):
+        j.sal.process.killProcessByName(self.NAME)
+
+    @builder_method()
+    def sandbox(self, zhub_client=None, flist_create=True, merge_base_flist=''):
+        bin_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, 'sandbox', 'bin')
+        lib_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, 'sandbox', 'lib')
+
+        bins = [
+            'capnp',
+            'capnp-afl-testcase',
+            'capnpc-c++',
+            'capnp-test',
+            'capnpc-capnp',
+            'capnp-evolution-test'
+        ]
+
+        libs = [
+            'libkj-http.so',
+            'libkj-async.so',
+            'libcapnpc.so',
+            'libkj-tls.so',
+            'libcapnp.so',
+            'libkj-test.so',
+            'libcapnp-json.so',
+            'libcapnp-rpc.so',
+            'libkj.so',
+            'libkj-async-0.8-dev.so',
+            'libkj-test-0.8-dev.so',
+            'libcapnpc-0.8-dev.so',
+            'libcapnp-json-0.8-dev.so',
+            'libcapnp-0.8-dev.so',
+            'libkj-0.8-dev.so',
+            'libkj-http-0.8-dev.so',
+            'libkj-tls-0.8-dev.so',
+            'libcapnp-rpc-0.8-dev.so'
+        ]
+        # copy bins
+        for bin in bins:
+            self.tools.dir_ensure(bin_dest)
+            bin_src = j.sal.fs.joinPaths('/sandbox/bin/', bin)
+            self._copy(bin_src, bin_dest)
+        
+        # copy libs
+        for lib in libs:
+            self.tools.dir_ensure(lib_dest)
+            lib_src = j.sal.fs.joinPaths('/sandbox/lib/', lib)
+            self._copy(lib_src, lib_dest)
+
+    @builder_method()
     def test(self):
         """
         kosmos 'j.builder.builder.libs.capnp.test()'
         """
-        self.build()
-
-        raise RuntimeError("implement")
-
+        return_code, _, _ = self._execute('capnp-test')
+        assert return_code == 0
         print("TEST OK")
