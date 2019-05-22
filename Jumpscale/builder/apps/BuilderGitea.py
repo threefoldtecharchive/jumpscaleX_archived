8110abb8551d99faed08c5ff78ba0a20aafbf580
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-05-22 16:43:37 +0200

    WIP: fix gitea build

--- Jumpscale/builder/apps/BuilderGitea.py
+++ Jumpscale/builder/apps/BuilderGitea.py
@@ -1,61 +1,55 @@
-from Jumpscale import j
 import textwrap
 
+from Jumpscale import j
+from Jumpscale.builder.runtimes.BuilderGolang import BuilderGolangTools
 
-class BuilderGitea(j.builder.system._BaseClass):
+builder_method = j.builder.system.builder_method
+
+
+class BuilderGitea(BuilderGolangTools):
     NAME = "gitea"
 
     def _init(self):
-        self._gogspath = str()
-        self._gopath = str()
-        self._appini = str()
+        super()._init()
+        # set needed paths
+        self.GITEAPATH = self._replace("{DIR_GO_PATH}/src/code.gitea.io/gitea")
+        self.CUSTOM_PATH = "%s/custom" % self.GITEAPATH
+        self.CODEDIR = self.GITEAPATH
+        self.INIPATH = "%s/conf/app.ini" % self.CUSTOM_PATH
 
-    @property
-    def GOPATH(self):
-        return j.builder.runtimes.golang.GOPATH
-
-    def build(self, reset=False):
+    @builder_method()
+    def build(self):
         """Build Gitea with itsyou.online config
 
         Keyword Arguments:
             reset {bool} -- force build if True (default: {False})
         """
-
-        # set needed paths
-        self.GITEAPATH = self._replace("{}/src/code.gitea.io/gitea".format(self.GOPATH))
-        self.CUSTOM_PATH = self._replace("%s/custom" % self.GITEAPATH)
-        self.CODEDIR = self.GITEAPATH
-        self.INIPATH = self._replace("%s/conf/app.ini" % self.CUSTOM_PATH)
-        if self._done_check("build", reset):
-            return
-
         self._installDeps()
-        j.builder.runtimes.golang.get("code.gitea.io/gitea")
+        self.get("code.gitea.io/gitea", install=False)
 
         # change branch from master to gigforks/iyo_integration
-        j.sal.process.execute(
-            "cd %s && git remote add gigforks https://github.com/gigforks/gitea" % self.GITEAPATH, profile=True
-        )
-        j.sal.process.execute(
-            "cd %s && git fetch gigforks && git checkout gigforks/iyo_integration" % self.GITEAPATH,
-            profile=True,
-            timeout=1200,
-        )
+        _, out, _ = self._execute("cd {GITEAPATH} && git remote")
+        if "gigforks" not in out:
+            self._execute(
+                """cd {GITEAPATH}
+                git remote add gigforks https://github.com/gigforks/gitea
+                git fetch gigforks && git checkout gigforks/iyo_integration
+            """
+            )
 
         # gitea-custom is needed to replace the default gitea custom
         j.clients.git.pullGitRepo("https://github.com/incubaid/gitea-custom.git", branch="master")
-        if not j.builder.tools.file_is_link(self.CUSTOM_PATH):
-            j.builder.tools.dir_remove(self.CUSTOM_PATH)
+        if self.tools.exists(self.CUSTOM_PATH) and not self.tools.file_is_link(self.CUSTOM_PATH):
+            self.tools.dir_remove(self.CUSTOM_PATH)
 
-        j.builder.tools.file_link(source="/sandbox/code/github/incubaid/gitea-custom", destination=self.CUSTOM_PATH)
+        self.tools.file_link(source="/sandbox/code/github/incubaid/gitea-custom", destination=self.CUSTOM_PATH)
 
+        # configure
+        self.tools.dir_ensure(self._replace("{CUSTOM_PATH}/conf"))
         self._configure()
 
         # build gitea (will be stored in self.GITEAPATH/gitea)
-        j.sal.process.execute(
-            'cd %s && TAGS="bindata" make generate build' % self.GITEAPATH, profile=True, timeout=1200
-        )
-        self._done_set("build")
+        self._execute('cd {GITEAPATH} && TAGS="bindata" make generate build')
 
     def _installDeps(self):
         """Install gitea deps
@@ -63,12 +57,11 @@ class BuilderGitea(j.builder.system._BaseClass):
         sys packages: git-core, gcc, golang
         db: postgresql
         """
-
         j.builder.system.package.mdupdate()
         j.builder.system.package.ensure("git-core")
         j.builder.system.package.ensure("gcc")
         j.builder.runtimes.golang.install()
-        j.builder.db.postgresql.install()
+        j.builder.db.postgres.install()
 
     def _configure(self):
         """Configure gitea
@@ -93,9 +86,10 @@ class BuilderGitea(j.builder.system._BaseClass):
         LEVEL     = Info
         """
         config = textwrap.dedent(config)
-        j.sal.fs.writeFile(location=self.INIPATH, content=config)
+        self._write(self.INIPATH, config)
 
-    def install(self, org_client_id, org_client_secret, reset=False, start=False):
+    @builder_method()
+    def install(self, org_client_id, org_client_secret):
         """Build Gitea with itsyou.online config
 
         Same as build but exists for standarization sake
@@ -103,16 +97,13 @@ class BuilderGitea(j.builder.system._BaseClass):
         Keyword Arguments:
             reset {bool} -- force build if True (default: {False})
         """
+        j.sal.process.killProcessByName("postgres")
+        j.builder.db.postgres.start()
+        self._execute("sudo -u postgres {DIR_BIN}/psql -c 'create database gitea;'")
 
-        self.build(reset=reset)
-        if not j.builder.db.postgresql.isStarted():
-            j.builder.db.postgresql.start()
+        self._execute("sudo -u postgres {DIR_BIN}/psql gitea -c")
 
-        # # Create postgres db  : #TODO:*1 this should be part of the postgresql prefab module
-        j.sal.process.execute("sudo -u postgres /opt/bin/psql -c 'create database gitea;'", die=False)
-        self.start()
         cmd = """
-        sudo -u postgres /opt/bin/psql gitea -c
         "INSERT INTO login_source (type, name, is_actived, cfg, created_unix, updated_unix)
         VALUES (6, 'Itsyou.online', TRUE,
         '{\\"Provider\\":\\"itsyou.online\\",\\"ClientID\\":\\"%s\\",\\"ClientSecret\\":\\"%s\\",\\"OpenIDConnectAutoDiscoveryURL\\":\\"\\",\\"CustomURLMapping\\":null}',
@@ -120,47 +111,11 @@ class BuilderGitea(j.builder.system._BaseClass):
         """
         cmd = cmd % (org_client_id, org_client_secret)
         cmd = cmd.replace("\n", " ").replace("\r", " ").replace("\t", " ")
-        j.sal.process.execute(cmd)
-        if not start:
-            self.stop()
-            j.builder.db.postgresql.stop()
+        self._execute(cmd)
 
-    def start(self, name="main"):
-        """Start GITEA server instance
+        self.tools.file_link(self._replace("{GITEAPATH}/gitea"), self.DIR_BIN)
 
-        Keyword Arguments:
-            name {string} -- name of the server instance (default: {'main'})
-        """
-        if not j.builder.db.postgresql.isStarted():
-            j.builder.db.postgresql.start()
-        cmd = "{giteapath}/gitea web".format(giteapath=self.GITEAPATH)
-        pm = j.builder.system.processmanager.get()
-        pm.ensure(name="gitea_%s" % name, cmd=cmd)
-
-    def stop(self, name="main"):
-        """Stop GITEA server instance
-
-        Keyword Arguments:
-            name {string} -- name of the server instance (default: {'main'})
-        """
-
-        pm = j.builder.system.processmanager.get()
-        pm.stop("gitea_%s" % name)
-
-    def restart(self, name="main"):
-        """Stop GITEA server instance
-
-        Keyword Arguments:
-            name {string} -- name of the server instance (default: {'main'})
-        """
-        pm = j.builder.system.processmanager.get()
-        pm.stop("gitea_%s" % name)
-        self.start(name)
-
-    def reset(self):
-        """
-        helper method to clean what this module generates.
-        """
-        super().reset()
-        j.sal.fs.remove(self.CODEDIR)
-        self._init()
+    @property
+    def startup_cmds(self):
+        cmd = j.tools.startupcmd.get("gitea", "gitea web", path="/sandbox/bin")
+        return [cmd]
