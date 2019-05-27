0035677e2679225722b497bc8223a5931808600c
Waleed Hammam <waleed.hammam@gmail.com>
2019-05-27 12:43:15 +0200

    reformat black

--- Jumpscale/clients/sshagent/SSHAgent.py
+++ Jumpscale/clients/sshagent/SSHAgent.py
@@ -72,14 +72,14 @@ class SSHAgent(j.application.JSBaseClass):
 
         _, out = get_key_list()
         keys = [line.split() for line in out.splitlines() if len(line.split()) == 3]
-        self._keys = list(map(lambda key: [key[2], ' '.join(key[0:2])], keys))
-        #get key_names loaded
+        self._keys = list(map(lambda key: [key[2], " ".join(key[0:2])], keys))
+        # get key_names loaded
         key_names = [j.sal.fs.getBaseName(i[0]) for i in self._keys]
 
         if "SSH_KEY_DEFAULT" not in j.core.myenv.config:
             j.core.myenv.config["SSH_KEY_DEFAULT"] = ""
 
-        if j.core.myenv.config["SSH_KEY_DEFAULT"] == "" and len(key_names)==1:
+        if j.core.myenv.config["SSH_KEY_DEFAULT"] == "" and len(key_names) == 1:
             j.core.myenv.config["SSH_KEY_DEFAULT"] = key_names[0]
             j.core.myenv.config_save()
 
