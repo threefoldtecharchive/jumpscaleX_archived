f612437a53f5521464bee8e9078e289a58b02d40
serboctor <boctor.sarah@gmail.com>
2019-06-03 13:45:33 +0200

    Add zhub test

--- Jumpscale/clients/zero_hub/ZeroHubFactory.py
+++ Jumpscale/clients/zero_hub/ZeroHubFactory.py
@@ -7,3 +7,16 @@ JSConfigs = j.application.JSBaseConfigsClass
 class ZeroHubFactory(JSConfigs):
     __jslocation__ = "j.clients.zhub"
     _CHILDCLASS = ZeroHubClient
+
+    def test(self):
+        """
+        js_shell 'j.clients.zhub.test()'
+        """
+        c = j.clients.zhub.test1
+        c.name = "test1"
+        c.token_ = "token"
+        c.username = "test"
+        c.save()
+
+        assert j.clients.zhub.test1.name == "test1"
+        assert j.clients.zhub.test1.username == "test"
