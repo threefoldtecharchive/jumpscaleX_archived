eb8f9ae57ce6a8d2cc45c0e2cb6f9f948b416227
kristof de spiegeleer <>
2019-06-12 21:40:10 +0200

    corex improvements

--- Jumpscale/clients/corex/CoreXClient.py
+++ Jumpscale/clients/corex/CoreXClient.py
@@ -12,29 +12,43 @@ class CoreXClient(JSConfigBase):
         self.host = host
         self.port = port
 
-    def endpoint(self, path):
+    def _endpoint(self, path):
+        """
+        :param path:
+        :return:
+        """
         return "http://%s:%d/api/%s" % (self.host, self.port, path)
 
     def process_list(self):
-        r = requests.get(self.endpoint("/processes")).json()
+        r = requests.get(self._endpoint("/processes")).json()
         return r["processes"]
 
     def process_clean(self):
-        r = requests.get(self.endpoint("/process/clean")).json()
+        """
+        If a process stop/die/whatever, it stay on the list to get its status
+        Clean remove all non running process from the list
+
+        :return:
+        """
+        r = requests.get(self._endpoint("/process/clean")).json()
         return r["status"] == "success"
 
     def process_start(self, args):
+        """
+        :param args: e.g. [‘mybib’, ‘-v’]
+        :return: id of the process
+        """
         if type(args) is not list:
             args = [args]
 
         params = {"arg[]": args}
-        r = requests.get(self.endpoint("/process/start"), params=params).json()
+        r = requests.get(self._endpoint("/process/start"), params=params).json()
         return r
 
     def process_logs(self, id):
-        r = requests.get(self.endpoint("/process/logs"), params={"id": id})
+        r = requests.get(self._endpoint("/process/logs"), params={"id": id})
         return r.text
 
     def process_stop(self, id):
-        r = requests.get(self.endpoint("/process/stop"), params={"id": id})
+        r = requests.get(self._endpoint("/process/stop"), params={"id": id})
         return r.json()
