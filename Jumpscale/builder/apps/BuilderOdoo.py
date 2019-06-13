2f0beb3c8d85868e1e7a96ebc8641ace2e88aa70
Abdelrahman Ghanem <abom.jdev@gmail.com>
2019-06-13 14:25:23 +0200

    odoo builder: add strings to identify odoo the process when stopping it

--- Jumpscale/builder/apps/BuilderOdoo.py
+++ Jumpscale/builder/apps/BuilderOdoo.py
@@ -66,6 +66,7 @@ class BuilderOdoo(j.builders.system._BaseClass):
 
     @property
     def startup_cmds(self):
+        # run the db with the same user when running odoo server
         pg_ctl = self._replace("sudo -u odoouser {DIR_BIN}/pg_ctl %s -D {APP_DIR}/data")
         cmd_start = pg_ctl % "start"
         cmd_stop = pg_ctl % "stop"
@@ -73,5 +74,7 @@ class BuilderOdoo(j.builders.system._BaseClass):
         odoo_start = self._replace(
             "sudo -H -u odoouser python3 /sandbox/apps/odoo/odoo/odoo-bin -c {DIR_CFG}/odoo.conf"
         )
-        odoo_cmd = j.tools.startupcmd.get("odoo", odoo_start, path="/sandbox/bin")
+        odoo_cmd = j.tools.startupcmd.get(
+            "odoo", odoo_start, process_strings=["/sandbox/apps/odoo/odoo/odoo-bin -c"], path="/sandbox/bin"
+        )
         return [postgres_cmd, odoo_cmd]
