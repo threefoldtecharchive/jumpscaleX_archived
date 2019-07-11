c41d2d40b8d7df0aa5f41608e9816b7c2d1349ce
BolaNasr <bola.nasr@live.com>
2019-07-11 18:36:37 +0200

    fix incorrect object returned by model.get #693

--- Jumpscale/data/bcdb/BCDBModel.py
+++ Jumpscale/data/bcdb/BCDBModel.py
@@ -428,10 +428,13 @@ class BCDBModel(j.application.JSBaseClass):
                 return None
 
         obj = self.bcdb._unserialize(obj_id, data, return_as_capnp=return_as_capnp, model=self)
+
+        if obj._schema.url == self.schema.url:
+            obj = self._triggers_call(obj=obj, action="get")
+        else:
+            raise RuntimeError("could not find obj with id:%s in your model" % obj_id)
+
         # self.obj_cache[obj_id] = (j.data.time.epoch, obj)  #FOR NOW NO CACHE, UNSAFE
-
-        obj = self._triggers_call(obj=obj, action="get")
-
         return obj
 
     def destroy(self, nid=1):
