c0bf51102ca0c727fdc0ed00459d876eaa912cbf
kristof de spiegeleer <>
2019-07-29 08:51:57 +0200

    licenser mods

--- Jumpscale/tools/licenser/FixerReplace.py
+++ Jumpscale/tools/licenser/FixerReplace.py
@@ -1,9 +1,18 @@
 from Jumpscale import j
 
+# THIS IS A LICENSE EDITING TOOLS THE BLOCKS BELOW IMPLY NOTHING RELATED TO LICENSES
+
+TFTECH = "# Copyright (C) July 2018:  TF TECH NV in Belgium see https://www.threefold.tech/"
+
+TFTECH_OTHER = """
+# Copyright (C) before July 2018 : companies related to Incubaid NV e.g Qlayer NV and GIG Technologies using Apache 2 license
+# Copyright (C) July 2018 :  TF TECH NV in Belgium see https://www.threefold.tech/, all modifications made by TF TECH NV are GPL v3
+"""
+
 
 GPL3 = """
-
-# Copyright (C) 2019 :  TF TECH NV in Belgium see https://www.threefold.tech/
+# In case TF TECH NV ceases to exist (e.g. because of bankruptcy) 
+#   then Incubaid NV also in Belgium will get the Copyright & Authorship for all changes made since July 2018 
 # This file is part of jumpscale at <https://github.com/threefoldtech>.
 # jumpscale is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,6 +26,7 @@ GPL3 = """
 #
 # You should have received a copy of the GNU General Public License
 # along with jumpscale or jumpscale derived works.  If not, see <http://www.gnu.org/licenses/>.
+# LICENSE END
 """
 
 
@@ -24,19 +34,52 @@ JSBASE = j.application.JSBaseClass
 
 
 class FixerReplacer(j.application.JSBaseClass):
-    def file_process(self, path, write=False):
+    def file_remove_license(self, C, tofind):
+        if C.find(tofind) != -1:
+            out = ""
+            state = "start"
+            for line in C.split("\n"):
+                if line.startswith(tofind):
+                    state = "license"
+                    continue
+                if state == "license":
+                    if line.strip() == "":
+                        continue
+                    else:
+                        state = "ok"
+                if state == "ok":
+                    out += line + "\n"
+            return True, out
+        return False, C
+
+    def file_process(self, path, write=False, header=None, addlicense=True):
+        assert header
         out = ""
         C = j.sal.fs.readFile(path)
-        if C.find("Copyright (C) 2019") != -1:
-            return
-        C = GPL3 + "\n\n" + C
+
+        rc = True
+        while rc:
+            rc, C = self.file_remove_license(C, "# LICENSE END")
+        rc = True
+        while rc:
+            rc, C = self.file_remove_license(C, "# You should ")
+        rc = True
+        while rc:
+            rc, C = self.file_remove_license(C, "# along with")
+        if addlicense:
+            C4 = header + GPL3 + "\n\n" + C
+        else:
+            C4 = C
         self._log_info("will copyright:%s" % path)
         if write:
-            j.sal.fs.writeFile(path, C)
+            j.sal.fs.writeFile(path, C4)
 
-    def dir_process(self, path, extensions=["py"], recursive=True, write=False):
+    def dir_process(self, path, extensions=["py"], recursive=True, write=False, header=None, addlicense=True):
+        if not header:
+            header = TFTECH
+        header = header.rstrip()
         path = j.sal.fs.pathNormalize(path)
         for ext in extensions:
             for p in j.sal.fs.listFilesInDir(path, recursive=recursive, filter="*.%s" % ext, followSymlinks=False):
                 self._log_debug("process file:%s" % p)
-                self.file_process(path=p, write=write)
+                self.file_process(path=p, write=write, header=header, addlicense=addlicense)
--- Jumpscale/tools/licenser/Licenser.py
+++ Jumpscale/tools/licenser/Licenser.py
@@ -20,18 +20,21 @@ class Licenser(j.application.JSBaseClass):
         JSBASE.__init__(self)
         self.replacer = FixerReplacer()
 
-    def do(self, path=None, write=False):
+    def do(self, path=None, write=False, addlicense=True):
         """
-        kosmos 'j.tools.licenser.do(write=True)'
+        kosmos 'j.tools.licenser.do(write=True,addlicense=False)'
         kosmos 'j.tools.licenser.do()'
         :param path:
         :param write:
         :return:
         """
         if not path:
-            self.do("/sandbox/code/github/threefoldtech/jumpscaleX", write=write)
-            self.do("/sandbox/code/github/threefoldtech/digitalmeX", write=write)
+            self.do("/sandbox/code/github/threefoldtech/jumpscaleX", write=write, addlicense=addlicense)
+            self.do("/sandbox/code/github/threefoldtech/digitalmeX", write=write, addlicense=addlicense)
         else:
             for path2 in j.sal.fs.listFilesInDir(path, filter=".gpl3", recursive=True):
+                header = j.sal.fs.readFile(path2)
+                if header.strip() == "":
+                    header = None
                 path3 = j.sal.fs.getDirName(path2)
-                self.replacer.dir_process(path=path3, write=write)
+                self.replacer.dir_process(path=path3, write=write, header=header, addlicense=addlicense)
