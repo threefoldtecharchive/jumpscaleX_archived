f98ee0cf4693751a109864dcaf5324df2ee3f70a
andrewayoub <andrew.a.mikhaeel@gmail.com>
2019-08-02 12:25:48 +0200

    fix image downloading in wikis

--- Jumpscale/data/bcdb/models_system/FILE.py
+++ Jumpscale/data/bcdb/models_system/FILE.py
@@ -118,7 +118,7 @@ class FILE(j.data.bcdb._BCDBModelClass):
         dir.save()
         return new_file
 
-    def file_write(self, path, content, append=True, create=True):
+    def file_write(self, path, content, append=False, create=True):
         """
         writes a file to bcdb
         :param path: the path to store the file
@@ -168,6 +168,7 @@ class FileStream:
     # plain types are the the file types that will be stored as plain text in content
     # other types will be saved in blocks
     PLAIN_TYPES = ["md", "txt", "json", "toml"]
+    BLOCK_SIZE = 8192
 
     def __init__(self, vfile):
         self._vfile = vfile
@@ -192,6 +193,21 @@ class FileStream:
             self._vfile.extension = ext
             self._vfile.save()
 
+    def _drain_response(self, response):
+        response_generator = response.iter_content(self.BLOCK_SIZE)
+        while True:
+            try:
+                yield next(response_generator)
+            except StopIteration:
+                yield None
+
+    def _drain_stream(self, stream):
+        block = stream.read(self.BLOCK_SIZE)
+        while block:
+            yield block
+            block = stream.read(self.BLOCK_SIZE)
+        yield None
+
     def _save_blocks(self, stream, append=True):
         if not append:
             for block_id in self._vfile.blocks:
@@ -199,7 +215,12 @@ class FileStream:
                 block.delete()
             self._vfile.blocks.clear()
             self._vfile.save()
-        block = stream.read1(8192)
+
+        if hasattr(stream, "iter_content"):
+            blocks_generator = self._drain_response(stream)
+        else:
+            blocks_generator = self._drain_stream(stream)
+        block = next(blocks_generator)
         while block:
             # hash = j.data.hash.md5_string(str(block) + str(self._vfile.id))
             # exists = self._block_model.find(md5=hash)
@@ -217,7 +238,7 @@ class FileStream:
             else:
                 self._vfile.size_bytes += exists[0].size
                 self._vfile.blocks.append(exists[0].id)
-            block = stream.read1(8192)
+            block = next(blocks_generator)
         self._vfile.save()
 
     def _save_plain(self, stream, append=True):
