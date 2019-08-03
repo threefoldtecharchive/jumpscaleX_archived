26b053a1ab60e4c97e34fe66f4f113728f6a4b18
hamdy <hamdy.a.farag@gmail.com>
2019-08-03 16:17:12 +0200

    markdown test in j.tools.markdowndocs.test2()

--- Jumpscale/tools/markdowndocs/MarkDownDocs.py
+++ Jumpscale/tools/markdowndocs/MarkDownDocs.py
@@ -335,6 +335,18 @@ class MarkDownDocs(j.application.JSBaseClass):
         ff_event_wiki = self.load(url, name="freeflowevent")
         ff_event_wiki.write()
 
+    def test2(self):
+        j.builders.runtimes.pip.install("webbrowser")
+
+        url = "https://github.com/threefoldtech/jumpscaleX/tree/development/docs/tools/wiki/docsites/examples/docs/"
+        examples = j.tools.markdowndocs.load(url, name="examples")
+        examples.write()
+
+        j.servers.threebot.get('test').start(background=True)
+
+        import webbrowser
+        webbrowser.open('localhost:8090/wiki/examples#/test_include')
+
     def test(self, watch=False):
         """
         kosmos 'j.tools.markdowndocs.test()'
