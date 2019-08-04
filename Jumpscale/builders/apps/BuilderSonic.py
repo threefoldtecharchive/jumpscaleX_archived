6421bd77307ff3d8c7cdd48be157723e00469714
Waleed Hammam <waleed.hammam@gmail.com>
2019-08-04 14:37:49 +0200

    add sonic sandbox

--- Jumpscale/builders/apps/BuilderSonic.py
+++ Jumpscale/builders/apps/BuilderSonic.py
@@ -38,3 +38,33 @@ class BuilderSonic(j.builders.system._BaseClass):
         :return:
         """
         self._execute("cp %s/sonic /sandbox/bin/" % j.builders.runtimes.rust.DIR_CARGOBIN)
+
+    @builder_method()
+    def sandbox(self, zhub_client=None, flist_create=True):
+        """Copy built bins to dest_path and reate flist if create_flist = True
+
+        :param dest_path: destination path to copy files into
+        :type dest_path: str
+        :param sandbox_dir: path to sandbox
+        :type sandbox_dir: str
+        :param reset: reset sandbox file transfer
+        :type reset: bool
+        :param create_flist: create flist after copying files
+        :type flist_create:bool
+        :param zhub_instance: hub instance to upload flist to
+        :type zhub_instance:str
+        """
+        # ensure dirs
+        bin_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, "sandbox", "bin")
+        self.tools.dir_ensure(bin_dest)
+
+        # sandbox zdb
+        bins = ["sonic"]
+        for bin in bins:
+            self._copy("{DIR_BIN}/" + bin, bin_dest)
+
+        lib_dest = j.sal.fs.joinPaths(self.DIR_SANDBOX, "sandbox", "lib")
+        self.tools.dir_ensure(lib_dest)
+        for bin in bins:
+            dir_src = self.tools.joinpaths(bin_dest, bin)
+            j.tools.sandboxer.libs_sandbox(dir_src, lib_dest)
