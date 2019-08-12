57fe482aae446553bf1445e2d22a5813ab8fbc79
Christophe de Carvalho <christophe.dcpm@gmail.com>
2019-08-12 10:18:30 +0200

    NACL: redo the nacl module (again)
    - we now store the seed used to generate keys
    - ed25519 singing key is use to derivate all the other keys (ed25519 verify key,
    curve25519 private and public key)
    - allow encrypt and decrypt method to use private and public key from
    other people. This is required to do asymetric encryption between 2
    users

--- Jumpscale/data/nacl/NACL.py
+++ Jumpscale/data/nacl/NACL.py
@@ -1,5 +1,7 @@
 from Jumpscale import j
 from nacl.public import PrivateKey, SealedBox
+from nacl.signing import SigningKey, VerifyKey
+from nacl.encoding import RawEncoder
 import nacl.signing
 import nacl.secret
 import nacl.utils
@@ -22,23 +24,53 @@ class NACL(j.application.JSBaseClass):
     def _init(self, name=None, **kwargs):
         assert name
         self.name = name
+        self._signingkey = None
+        self._box = None
         if not Tools.exists(self._path):
             Tools.dir_ensure(self._path)
 
     def reset(self):
+        self._signingkey = None
         self._box = None
+
+    # we expose the signingkey as a property to
+    # make it read-only and prevent the user to overwrite it
+    @property
+    def signing_key(self):
+        return self._signingkey
+
+    @property
+    def verify_key(self):
+        return self.signing_key.verify_key
+
+    @property
+    def private_key(self):
+        return self.signing_key.to_curve25519_private_key()
+
+    @property
+    def public_key(self):
+        return self.signing_key.verify_key.to_curve25519_public_key()
+
+    @property
+    def box(self):
+        """
+        box is a used to do symetric encryption
+        it uses the local private key
+        """
+        if not self._box:
+            self.load()
+        return self._box
 
     @property
     def _path(self):
         return "%s/keys/%s" % (MyEnv.config["DIR_CFG"], self.name)
 
     @property
-    def _path_privatekey(self):
+    def _path_seed(self):
         return "%s/key.priv" % (self._path)
 
     def _ask_privkey_words(self):
         """
-
         :param privkey_words:
         :return:
         """
@@ -54,7 +86,7 @@ class NACL(j.application.JSBaseClass):
             print("{BLUE}" + self.words + "{RESET}\n")
             print("\n{RED}ITS IMPORTANT TO STORE THIS KEY IN A SAFE PLACE{RESET}")
             if not Tools.ask_yes_no("Did you write the words down and store them in safe place?"):
-                j.sal.fs.remove(self._path_privatekey)
+                j.sal.fs.remove(self._path_seed)
                 print("WE HAVE REMOVED THE KEY, need to restart this procedure.")
                 sys.exit(1)
             j.tools.console.clear_screen()
@@ -71,7 +103,7 @@ class NACL(j.application.JSBaseClass):
                 print("the control word was not correct, please try again")
                 return self.word3_check(word3)
         except KeyboardInterrupt:
-            j.sal.fs.remove(self._path_privatekey)
+            j.sal.fs.remove(self._path_seed)
             print("WE HAVE REMOVED THE KEY, need to restart this procedure.")
 
     @property
@@ -80,27 +112,33 @@ class NACL(j.application.JSBaseClass):
         e.g.
         kosmos 'print(j.data.nacl.default.words)'
         """
-        assert self.privkey is not None
-        privkey = self.privkey.encode()
-        return j.data.encryption.mnemonic.to_mnemonic(privkey)
+        if self._signingkey is None:
+            raise j.exceptions.NotFound("seed not found, generate a new key pair fist")
+        seed = self.signing_key._seed
+        return j.data.encryption.mnemonic.to_mnemonic(seed)
 
     def _keys_generate(self, words=None):
         """
-        Generate private key (strong) & store in chosen path encrypted using the local secret
+        Generate an ed25519 signing key
+        if words if specified, words are used as seed to rengerate a known key
+        if words is None, a random seed is generated
+        
+        once the key is generated it is stored in chosen path encrypted using the local secret
         """
         if words:
-            key2 = j.data.encryption.mnemonic.to_entropy(words)
+            seed = j.data.encryption.mnemonic.to_entropy(words)
+            key = SigningKey(seed)
         else:
-            key = PrivateKey.generate()
-            key2 = key.encode()  # generates a bytes representation of the key
-        key3 = self.encryptSymmetric(key2)
-        self._file_write_hex(self._path_privatekey, key3)
+            key = SigningKey.generate()
+            seed = key._seed
+
+        encrypted_seed = self.encryptSymmetric(seed)
+        self._file_write_hex(self._path_seed, encrypted_seed)
 
         # build in verification
-        key4 = self._file_read_hex(self._path_privatekey)
-        assert key3 == key4
+        assert encrypted_seed == self._file_read_hex(self._path_seed)
 
-        self._load_privatekey()
+        self._load_singing_key()
 
     def configure(self, privkey_words=None, sshagent_use=None, interactive=None, generate=True):
         """
@@ -118,8 +156,6 @@ class NACL(j.application.JSBaseClass):
         """
         Tools.log("NACL uses path:'%s'" % self._path)
 
-        self.privkey = None
-
         if interactive is None:
             interactive = j.application.interactive
 
@@ -128,17 +164,17 @@ class NACL(j.application.JSBaseClass):
 
         self.load(die=False)
 
-        if self.privkey is None:
+        if self._signingkey is None:
             if privkey_words:
                 self._keys_generate(words=privkey_words)
             else:
-                if j.sal.fs.exists(self._path_privatekey):
-                    self._log_info("load existing private key")
+                if j.sal.fs.exists(self._path_seed):
+                    self._log_info("load existing key")
                     self.load()
-                    assert self.privkey
+                    assert self._signingkey
                     return
+                # means we did not find the seed yet
                 if interactive:
-                    # means we did not find a priv key yet
                     self._ask_privkey_words()
                 elif generate:
                     self._keys_generate()
@@ -147,17 +183,11 @@ class NACL(j.application.JSBaseClass):
 
             self.load(die=True)
 
-    def _error_raise(self, msg):
-        raise j.exceptions.Base(msg)
-
     def load(self, die=True):
         """
         will load private key from filesystem
         if not possible will exit to shell
         """
-        self._signingkey = ""
-        self.privkey = None
-
         if not j.core.myenv.config["SECRET"]:
             raise j.exceptions.Base("secret should already have been set, do 'jsx check'")
             # j.core.myenv.secret_set() #there is no secret yet
@@ -175,40 +205,34 @@ class NACL(j.application.JSBaseClass):
 
         try:
             self._box = nacl.secret.SecretBox(key)  # used to decrypt the private key
-        except nacl.exceptions.CryptoError as e:
+        except nacl.exceptions.CryptoError:
             if die:
                 self._error_raise("could not use the secret key, maybe wrong one, please use 'jsx check'.")
             else:
                 return False
 
-        return self._load_privatekey(die=die)
+        return self._load_singing_key(die=die)
 
-    def _load_privatekey(self, die=True):
+    def _load_singing_key(self, die=True):
 
-        if not j.sal.fs.exists(self._path_privatekey):
+        if not j.sal.fs.exists(self._path_seed):
             if die:
                 self._error_raise("could not find the path of the private key, please use 'jsx check' to fix.")
             else:
                 return False
-        priv_key = self._file_read_hex(self._path_privatekey)
+
+        seed = self._file_read_hex(self._path_seed)
 
         try:
-            priv_key_decrypted = self.decryptSymmetric(priv_key)
-        except nacl.exceptions.CryptoError as e:
+            seed = self.decryptSymmetric(seed)
+        except nacl.exceptions.CryptoError:
             if die:
                 self._error_raise("could not decrypt the private key, maybe wrong one, please use 'jsx check' to fix.")
             else:
                 return False
 
-        self.privkey = PrivateKey(priv_key_decrypted)
-
+        self._signingkey = SigningKey(seed)
         return True
-
-    @property
-    def box(self):
-        if not self._box:
-            self.load()
-        return self._box
 
     def _hash(self, data):
         m = hashlib.sha256()
@@ -216,22 +240,6 @@ class NACL(j.application.JSBaseClass):
             data = data.encode()
         m.update(data)
         return m.digest()
-
-    @property
-    def pubkey(self):
-        return self.privkey.public_key
-
-    @property
-    def signingkey(self):
-        if self._signingkey == "":
-            encrypted_key = self._file_read_hex(self._path_privatekey)
-            key = self.decryptSymmetric(encrypted_key)
-            self._signingkey = nacl.signing.SigningKey(key)
-        return self._signingkey
-
-    @property
-    def signingkey_pub(self):
-        return self.signingkey.verify_key
 
     def tobytes(self, data):
         if not j.data.types.bytes.check(data):
@@ -259,14 +267,9 @@ class NACL(j.application.JSBaseClass):
         data2 = self.sign_with_ssh_key(data)
         return j.data.hash.md5_string(data2)
 
-    def encryptSymmetric(self, data, hex=False, salt=""):
-
+    def encryptSymmetric(self, data, hex=False):
         box = self.box
-        if salt == "":
-            salt = nacl.utils.random(nacl.secret.SecretBox.NONCE_SIZE)
-        else:
-            salt = j.data.hash.md5_string(salt)[0:24].encode()
-        res = box.encrypt(self.tobytes(data), salt)
+        res = box.encrypt(self.tobytes(data))
         if hex:
             res = self._bin_to_hex(res).decode()
         return res
@@ -277,24 +280,32 @@ class NACL(j.application.JSBaseClass):
         res = self._box.decrypt(self.tobytes(data))
         return res
 
-    def encrypt(self, data, hex=False):
+    def encrypt(self, data, hex=False, public_key=None):
         """ Encrypt data using the public key
             :param data: data to be encrypted, should be of type binary
+            :param public_key: if None, the local public key is used
             @return: encrypted data
         """
-        data = self.tobytes(data)
-        sealed_box = SealedBox(self.pubkey)
+        if not public_key:
+            public_key = self.public_key
+
+            data = self.tobytes(data)
+        sealed_box = SealedBox(self.public_key)
         res = sealed_box.encrypt(data)
         if hex:
             res = self._bin_to_hex(res)
         return res
 
-    def decrypt(self, data, hex=False):
+    def decrypt(self, data, hex=False, private_key=None):
         """ Decrypt incoming data using the private key
             :param data: encrypted data provided
+            :param private_key: if None the local private key is used
             @return decrypted data
         """
-        unseal_box = SealedBox(self.privkey)
+        if not private_key:
+            private_key = self.private_key
+
+        unseal_box = SealedBox(self.private_key)
         if hex:
             data = self._hex_to_bin(data)
         return unseal_box.decrypt(data)
@@ -304,22 +315,22 @@ class NACL(j.application.JSBaseClass):
         sign using your private key using Ed25519 algorithm
         the result will be 64 bytes
         """
-        signed = self.signingkey.sign(data)
+        signed = self.signing_key.sign(data)
         return signed.signature
 
-    def verify(self, data, signature, pubkey=""):
+    def verify(self, data, signature, verify_key=""):
         """ data is the original data we have to verify with signature
             signature is Ed25519 64 bytes signature
-            pubkey is the signature public key, is not specified will use
-            your own (the pubkey is 32 bytes)
-
+            verify_key is the verify key, is not specified will use
+            your own (the verify key is 32 bytes)
         """
-        if pubkey == "":
-            pubkey = self.signingkey_pub
-        else:
-            pubkey = nacl.signing.VerifyKey(pubkey)
+        if verify_key is None or verify_key == "":
+            verify_key = self.verify_key
+        elif j.data.types.bytes.check(verify_key):
+            verify_key = VerifyKey(verify_key)
+
         try:
-            pubkey.verify(data, signature)
+            verify_key.verify(data, signature)
         except BadSignatureError:
             return False
 
@@ -355,6 +366,9 @@ class NACL(j.application.JSBaseClass):
     def _hex_to_bin(self, content):
         content = binascii.unhexlify(content)
         return content
+
+    def _error_raise(self, msg):
+        raise j.exceptions.Base(msg)
 
     def __str__(self):
         return "nacl:%s" % self.name
